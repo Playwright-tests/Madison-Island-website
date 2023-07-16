bc10eb565421d63afda534bd19e39a7ca7f04383
aksi1984 <pablo_xmen@op.pl>
2023-07-16 14:07:29 +0200

    SearchEngine class implementation

---
+++ src/test/java/pages/components/SearchEngine.java
@@ -0,0 +1,30 @@
+package pages.components;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import qa.base.BasePage;
+
+public class SearchEngine extends BasePage {
+
+    private final Locator searchField;
+    private final Locator searchButton;
+
+    public SearchEngine(Page page) {
+
+        super(page);
+
+        searchField = getPage().locator("#search");
+        searchButton = getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search"));
+    }
+
+    public void setPhrase(String phrase) {
+
+        searchField.fill(phrase);
+    }
+
+    public void clickSearchButton() {
+
+        searchButton.click();
+    }
+}
