fc2440157546de11c114bb0b2f03fb37b1e80473
aksi1984 <pablo_xmen@op.pl>
2023-07-16 13:57:30 +0200

    FooterLinks class implementation

---
+++ src/test/java/pages/components/FooterLinks.java
@@ -0,0 +1,18 @@
+package pages.components;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import qa.base.BasePage;
+
+
+public class FooterLinks extends BasePage {
+
+    public FooterLinks(Page page) {
+
+        super(page);
+    }
+    public void clickElementList(String linkText) {
+
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(linkText).setExact(true)).click();
+    }
+}
