cfb3df3c97972f5d6124a54482ddbe2525e3821b
aksi1984 <pablo_xmen@op.pl>
2023-07-30 23:21:19 +0200

    Changed annotation from @BeforeAll to @BeforeClass

--- src/test/java/qa/base/BaseTest.java
+++ src/test/java/qa/base/BaseTest.java
@@ -4,9 +4,8 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.BeforeClass;
+import utils.JSONReader;
 
 public class BaseTest {
 
@@ -14,13 +13,15 @@ public class BaseTest {
     private static Browser browser;
     private static Page page;
 
-    @BeforeAll
+    @BeforeClass
     public static void launch() {
 
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
         page = browser.newPage();
         page.navigate("http://demo-store.seleniumacademy.com/");
+
+        JSONReader.read();
     }
 
     public static Page getPage() {
