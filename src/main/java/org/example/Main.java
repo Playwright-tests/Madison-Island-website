251bda4907f6ca5ffed5422dfdf5b95c166e2c21
aksi1984 <pablo_xmen@op.pl>
2023-07-31 13:45:48 +0200

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
