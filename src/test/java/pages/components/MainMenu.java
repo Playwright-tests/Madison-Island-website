0d4b38cb08f5b2ae29ec3254f71830d8dab5b131
aksi1984 <pablo_xmen@op.pl>
2023-08-07 23:46:12 +0200

    Added necessary locators

--- src/test/java/pages/components/MainMenu.java
+++ src/test/java/pages/components/MainMenu.java
@@ -7,25 +7,72 @@ import qa.base.BasePage;
 
 public class MainMenu extends BasePage {
 
-    Locator dropdownList;
+    Locator parent;
+    Locator womenItems;
+    Locator menItems;
+    Locator accessoriesItems;
+    Locator homeAndDecorItems;
+    Locator saleItems;
+
     public MainMenu(Page page) {
 
         super(page);
+
+        parent = getPage().locator("#nav");
+
+        womenItems = getPage().getByRole(AriaRole.LISTITEM)
+                .filter(new Locator.FilterOptions().setHasText("WomenView All WomenNew ArrivalsTops & BlousesPants & DenimDresses & Skirts"));
+
+        menItems = getPage().getByRole(AriaRole.LISTITEM)
+                .filter(new Locator.FilterOptions().setHasText("MenView All MenNew ArrivalsShirtsTees, Knits and PolosPants & DenimBlazers"));
+
+        accessoriesItems = getPage().getByRole(AriaRole.LISTITEM)
+                .filter(new Locator.FilterOptions().setHasText("AccessoriesView All AccessoriesEyewearJewelryShoesBags & Luggage"));
+
+        homeAndDecorItems = getPage().getByRole(AriaRole.LISTITEM)
+                .filter(new Locator.FilterOptions().setHasText("Home & DecorView all Home & DecorBooks & MusicBed & BathElectronicsDecorative Accents"));
+
+        saleItems = getPage().getByRole(AriaRole.LISTITEM)
+                .filter(new Locator.FilterOptions().setHasText("SaleView All SaleWomenMenAccessoriesHome & Decor"));
     }
 
-    public void setDropdownList(String locator) {
+    public void hoverParent(String name) {
 
-        dropdownList = getPage().locator("#nav").locator(locator).locator("li");
+        parent.getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName(name).setExact(true)).hover();
     }
 
-    public int getDropdownListCount() {
+    private void clickItem(Locator locator, String name) {
 
-        return dropdownList.count();
+        locator.getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName(name).setExact(true)).click();;
     }
 
-    public void clickElementList(String name, int nthChild) {
+    public void clickWomenItem(String name) {
 
-        getPage().locator("#nav").getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName(name).setExact(true)).hover();
-        dropdownList.nth(nthChild).click();
+        clickItem(womenItems, name);
+    }
+
+    public void clickMenItem(String name) {
+
+        clickItem(menItems, name);
+    }
+
+    public void clickAccessoriesItem(String name) {
+
+        clickItem(accessoriesItems, name);
+    }
+
+    public void clickHomeAndDecorItem(String name) {
+
+        clickItem(homeAndDecorItems, name);
+    }
+
+    public void clickSaleItem(String name) {
+
+        clickItem(saleItems, name);
+    }
+
+    public void clickVip() {
+
+        parent.getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("VIP").setExact(true)).click();
     }
 }
