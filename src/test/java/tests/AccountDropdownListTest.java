e303eda1a8317b7c4323177a43fba6bdf079ebc7
aksi1984 <pablo_xmen@op.pl>
2023-08-24 08:40:54 +0200

    Added assertions and expectedResults data

--- src/test/java/tests/AccountDropdownListTest.java
+++ src/test/java/tests/AccountDropdownListTest.java
@@ -1,60 +1,70 @@
 package tests;
 
+import enums.AccountDropdownListURLs;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import pages.components.AccountDropdownList;
 import qa.base.BaseTest;
+import utils.JSONReader;
 
 public class AccountDropdownListTest extends BaseTest {
 
     private AccountDropdownList accountDropdownList;
+    private String[] expectedResults;
 
     @BeforeClass
     public void init() {
 
         accountDropdownList = new AccountDropdownList(getPage());
+        expectedResults = JSONReader.get("URLs", "accountDropdownList");
     }
 
-    private void clickTheLink(String items) {
+    private void check(String items, AccountDropdownListURLs index) {
 
         accountDropdownList.clickAccountNav();
         accountDropdownList.clickItem(items);
+
+        String currentURL = getPage().url();
+
         getPage().goBack();
+
+        Assert.assertEquals(currentURL, expectedResults[index.ordinal()]);
     }
 
     @Test
     void myAccountLink() {
 
-        clickTheLink("My Account");
+        check("My Account", AccountDropdownListURLs.MY_ACCOUNT);
     }
 
     @Test
     void myWishlistLink() {
 
-        clickTheLink("My Wishlist");
+        check("My Wishlist", AccountDropdownListURLs.MY_WISHLIST);
     }
 
     @Test
     void myCartLink() {
 
-        clickTheLink("My Cart");
+        check("My Cart", AccountDropdownListURLs.MY_CART);
     }
 
     @Test
     void checkoutLink() {
 
-        clickTheLink("Checkout");
+        check("Checkout", AccountDropdownListURLs.CHECKOUT);
     }
 
     @Test
     void registerLink() {
 
-        clickTheLink("Register");
+        check("Register", AccountDropdownListURLs.REGISTER);
     }
 
     @Test
     void logInLink() {
 
-        clickTheLink("Log In");
+        check("Log In", AccountDropdownListURLs.LOG_IN);
     }
 }
