6015fbc66f99390af810c08458a352d424b9d99c
aksi1984 <pablo_xmen@op.pl>
2023-09-13 18:57:04 +0200

    Added test data and assertions

--- src/test/java/tests/MainMenuTest.java
+++ src/test/java/tests/MainMenuTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import dataProvider.Provider;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import pages.sections.Header;
 import qa.base.BaseTest;
-import utils.Function;
+import java.util.function.Consumer;
 
 public class MainMenuTest extends BaseTest {
 
@@ -16,58 +18,59 @@ public class MainMenuTest extends BaseTest {
         header = new Header(getPage());
     }
 
-    private void clickItem(Function function, String[] items) {
+    private void clickItem(Consumer<String> consumer, String[] items, String[] URLs) {
 
+        for (int i = 0; i < items.length; i++) {
 
-        for (String item : items) {
+            consumer.accept(items[i]);
 
-            function.run(item);
+            Assert.assertEquals(getPage().url(), URLs[i]);
         }
     }
 
-    @Test
-    public void womenDropdownList() {
+    @Test(dataProvider = "mainMenuWomen", dataProviderClass = Provider.class)
+    public void womenDropdownList(String[] URLs) {
 
         String[] items = {"View All Women", "New Arrivals", "Tops & Blouses", "Pants & Denim", "Dresses & Skirts"};
 
         clickItem((x)->{ header.getMainMenu().hoverParent("Women");
-                         header.getMainMenu().clickWomenItem(x); }, items);
+                         header.getMainMenu().clickWomenItem(x); }, items, URLs);
     }
 
-    @Test
-    public void menDropdownList() {
+    @Test(dataProvider = "mainMenuMen", dataProviderClass = Provider.class)
+    public void menDropdownList(String[] URLs) {
 
         String[] items = {"View All Men", "New Arrivals", "Shirts", "Tees, Knits and Polos", "Pants & Denim", "Blazers"};
 
         clickItem((x)->{ header.getMainMenu().hoverParent("Men");
-                         header.getMainMenu().clickMenItem(x); }, items);
+                         header.getMainMenu().clickMenItem(x); }, items, URLs);
     }
 
-    @Test
-    public void accessoriesDropdownList() {
+    @Test(dataProvider = "mainMenuAccessories", dataProviderClass = Provider.class)
+    public void accessoriesDropdownList(String[] URLs) {
 
         String[] items = {"View All Accessories", "Eyewear", "Jewelry", "Shoes", "Bags & Luggage"};
 
         clickItem((x)->{ header.getMainMenu().hoverParent("Accessories");
-                         header.getMainMenu().clickAccessoriesItem(x); }, items);
+                         header.getMainMenu().clickAccessoriesItem(x); }, items, URLs);
     }
 
-    @Test
-    void homeAndDecorDropdownList() {
+    @Test(dataProvider = "mainMenuHomeDecor", dataProviderClass = Provider.class)
+    void homeAndDecorDropdownList(String[] URLs) {
 
         String[] items = {"View All Home & Decor", "Books & Music", "Bed & Bath", "Electronics", "Decorative Accents"};
 
         clickItem((x)->{ header.getMainMenu().hoverParent("Home & Decor");
-                         header.getMainMenu().clickHomeAndDecorItem(x); }, items);
+                         header.getMainMenu().clickHomeAndDecorItem(x); }, items, URLs);
     }
 
-    @Test
-    void saleDropdownList() {
+    @Test(dataProvider = "mainMenuSale", dataProviderClass = Provider.class)
+    void saleDropdownList(String[] URLs) {
 
         String[] items = {"View All Sale", "Women", "Men", "Accessories", "Home & Decor"};
 
         clickItem((x)->{ header.getMainMenu().hoverParent("Sale");
-                         header.getMainMenu().clickSaleItem(x); }, items);
+                         header.getMainMenu().clickSaleItem(x); }, items, URLs);
     }
 
     @Test
