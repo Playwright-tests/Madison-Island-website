b09826eccbda07532df5fc76df2114be38468e17
aksi1984 <pablo_xmen@op.pl>
2023-09-14 23:09:18 +0200

    Added assertion

--- src/test/java/tests/NewsletterTest.java
+++ src/test/java/tests/NewsletterTest.java
@@ -1,35 +1,51 @@
 package tests;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import dataProvider.Provider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import pages.sections.Footer;
 import qa.base.BaseTest;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import java.util.function.Consumer;
 
 public class NewsletterTest extends BaseTest {
 
     private static Footer footer;
 
-    @BeforeAll
+    @BeforeClass
     public static void init() {
 
         footer = new Footer(getPage());
     }
 
-    @Test
-    public void incorrectName() {
+    private void check(String[] emails, Consumer<Footer> consumer) {
 
-        footer.getNewsletterForm().setName("@@#$");
+        for (String email: emails) {
+
+            footer.getNewsletterForm().setEmail(email);
+            footer.getNewsletterForm().clickSubscribeButton();
+
+            consumer.accept(footer);
+        }
     }
 
-    @Test
-    public void emptyNameField() {
+    @Test(dataProvider = "newsletterIncorrectEmail", dataProviderClass = Provider.class)
+    public void incorrectName(String[] emails) {
 
-        footer.getNewsletterForm().setName("");
+
     }
 
-    @Test
-    public void correctName() {
+    @Test(dataProvider = "newsletterEmptyEmailField", dataProviderClass = Provider.class)
+    public void emptyNameField(String[] emails) {
 
-        footer.getNewsletterForm().setName("Paul");
+        check(emails, (Footer f)->{assertThat(f.getNewsletterForm().getAdviceRequiredEmail()).isVisible();});
+    }
+
+    @Test(dataProvider = "newsletterCorrectEmail", dataProviderClass = Provider.class)
+    public void correctName(String[] emails) {
+
+        check(emails, (Footer f)->{Assert.assertEquals(f.getNewsletterForm().getEmailFieldText(), "");});
     }
 }
