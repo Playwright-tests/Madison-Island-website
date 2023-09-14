a7d826ac2ce62a2f3243f1344bc9953fa97a7f54
aksi1984 <pablo_xmen@op.pl>
2023-09-14 08:32:28 +0200

    Added assertions

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,14 +8,12 @@ import pages.components.AccountDropdownList;
 import pages.components.LoginForm;
 import qa.base.BaseTest;
 import utils.Pair;
-
 import java.util.List;
+import java.util.function.Consumer;
 
 public class LoginTest extends BaseTest {
 
     private LoginForm loginForm;
-
-    private final String expectedMessage = "This is a required field.";
 
     @BeforeClass
     public void init() {
@@ -27,38 +25,35 @@ public class LoginTest extends BaseTest {
         accountDropdownList.clickItem("My Account");
     }
 
-    private void setData(String email, String password) {
+    private void setData(String email, String password, Consumer<LoginForm> consumer) {
 
         loginForm.setEmail(email);
         loginForm.setPassword(password);
         loginForm.clickLoginButton();
+
+        consumer.accept(loginForm);
     }
 
     @Test(dataProvider = "incorrectEmail", dataProviderClass = Provider.class)
     public void incorrectEmail(List<Pair<String, String>> data) {
 
-        setData(data.get(0).getFirst(), data.get(0).getSecond());
     }
 
     @Test(dataProvider = "emptyEmailField", dataProviderClass = Provider.class)
     public void emptyEmailField(List<Pair<String, String>> data) {
 
-        setData(data.get(0).getFirst(), data.get(0).getSecond());
-
-        Assert.assertEquals(loginForm.getRequiredEmailMessageText(), expectedMessage);
+        setData(data.get(0).getFirst(), data.get(0).getSecond(), (LoginForm lf)->{Assert.assertTrue(lf.isRequiredEmailMessageVisible());});
     }
 
     @Test(dataProvider = "incorrectPassword", dataProviderClass = Provider.class)
     public void incorrectPassword(List<Pair<String, String>> data) {
 
-        setData(data.get(0).getFirst(), data.get(0).getSecond());
+        setData(data.get(0).getFirst(), data.get(0).getSecond(), (LoginForm lf)->{Assert.assertTrue(lf.isInvalidLoginOrPasswordMessageVisible());});
     }
 
     @Test(dataProvider = "emptyPasswordField", dataProviderClass = Provider.class)
     public void emptyPasswordField(List<Pair<String, String>> data) {
 
-        setData(data.get(0).getFirst(), data.get(0).getSecond());
-
-        Assert.assertEquals(loginForm.getRequiredPasswordMessageText(), expectedMessage);
+        setData(data.get(0).getFirst(), data.get(0).getSecond(), (LoginForm lf)->{Assert.assertTrue(lf.isRequiredPasswordMessageVisible());});
     }
 }
