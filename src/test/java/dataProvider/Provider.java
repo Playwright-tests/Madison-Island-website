70f204066758dd82dd05c0e1e0177d59ca88f731
aksi1984 <pablo_xmen@op.pl>
2023-10-03 22:31:13 +0200

    Added data provider

--- src/test/java/dataProvider/Provider.java
+++ src/test/java/dataProvider/Provider.java
@@ -4,168 +4,127 @@ import org.testng.annotations.DataProvider;
 import utils.JSONReader;
 import utils.Pair;
 
-import java.util.List;
-
 public class Provider {
+
+    @DataProvider(name = "accountDropdownList")
+    public Object[] accountDropdownList() {
+
+        return JSONReader.get("URLs", "accountDropdownList", new Pair<>("button", "url"));
+    }
 
     @DataProvider(name = "incorrectEmail")
     public Object[] incorrectEmail() {
 
-        List<Pair<String, String>> data = JSONReader.get("login", "incorrectEmail", new Pair<>("email", "password"));
-
-        return new Object[] {data};
+        return JSONReader.get("login", "incorrectEmail", new Pair<>("email", "password"));
     }
 
     @DataProvider(name = "emptyEmailField")
     public Object[] emptyEmailField() {
 
-        List<Pair<String, String>> data = JSONReader.get("login", "emptyEmailField", new Pair<>("email", "password"));
-
-        return new Object[] {data};
+        return JSONReader.get("login", "emptyEmailField", new Pair<>("email", "password"));
     }
 
     @DataProvider(name = "incorrectPassword")
     public Object[] incorrectPassword() {
 
-        List<Pair<String, String>> data = JSONReader.get("login", "incorrectPassword", new Pair<>("email", "password"));
-
-        return new Object[] {data};
+        return JSONReader.get("login", "incorrectPassword", new Pair<>("email", "password"));
     }
 
     @DataProvider(name = "emptyPasswordField")
     public Object[] emptyPasswordField() {
 
-        List<Pair<String, String>> data = JSONReader.get("login", "emptyPasswordField", new Pair<>("email", "password"));
-
-        return new Object[] {data};
+        return JSONReader.get("login", "emptyPasswordField", new Pair<>("email", "password"));
     }
 
     @DataProvider(name = "mainMenuWomen")
     public Object[] mainMenuWomen() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuWomen");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuWomen", new Pair<>("button", "url"));
     }
 
     @DataProvider(name = "mainMenuMen")
     public Object[] mainMenuMen() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuMen");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuMen", new Pair<>("button", "url"));
     }
 
     @DataProvider(name = "mainMenuAccessories")
     public Object[] mainMenuAccessories() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuAccessories");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuAccessories", new Pair<>("button", "url"));
     }
 
     @DataProvider(name = "mainMenuHomeDecor")
     public Object[] mainMenuHomeDecor() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuHomeDecor");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuHomeDecor", new Pair<>("button", "url"));
     }
 
     @DataProvider(name = "mainMenuSale")
     public Object[] mainMenuSale() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuSale");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuSale", new Pair<>("button", "url"));
     }
 
     @DataProvider(name = "mainMenuVIP")
     public Object[] mainMenuVIP() {
 
-        String[] data = JSONReader.get("URLs", "mainMenuVIP");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "mainMenuVIP");
     }
 
     @DataProvider(name = "footerCompany")
     public Object[] footerCompany() {
 
-        String[] data = JSONReader.get("URLs", "footer_1");
+        return JSONReader.get("URLs", "footerCompany", new Pair<>("link", "url"));
 
-        return new Object[] {data};
     }
 
     @DataProvider(name = "footerQuickLinks")
     public Object[] footerQuickLinks() {
 
-        String[] data = JSONReader.get("URLs", "footer_2");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "footerQuickLinks", new Pair<>("link", "url"));
     }
 
     @DataProvider(name = "footerAccount")
     public Object[] footerAccount() {
 
-        String[] data = JSONReader.get("URLs", "footer_3");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "footerAccount", new Pair<>("link", "url"));
     }
 
     @DataProvider(name = "footerConnectWithUs")
     public Object[] footerConnectWithUs() {
 
-        String[] data = JSONReader.get("URLs", "footer_4");
-
-        return new Object[] {data};
+        return JSONReader.get("URLs", "footerConnectWithUs", new Pair<>("link", "url"));
     }
 
     @DataProvider(name = "searchEngineCorrectPhrase")
     public Object[] searchEngineCorrectPhrase() {
 
-        String[] data = JSONReader.get("searchEngine", "correctPhrase");
-
-        return new Object[] {data};
+        return JSONReader.get("searchEngine", "correctPhrase");
     }
 
     @DataProvider(name = "searchEngineCorrectPhraseUpperLower")
     public Object[] searchEngineCorrectPhraseUpperLower() {
 
-        String[] data = JSONReader.get("searchEngine", "correctPhraseUpperLower");
-
-        return new Object[] {data};
+        return JSONReader.get("searchEngine", "correctPhraseUpperLower");
     }
 
     @DataProvider(name = "searchEngineIncorrectPhrase")
     public Object[] searchEngineIncorrectPhrase() {
 
-        String[] data = JSONReader.get("searchEngine", "incorrectPhrase");
-
-        return new Object[] {data};
+        return JSONReader.get("searchEngine", "incorrectPhrase");
     }
 
     @DataProvider(name = "newsletterIncorrectEmail")
     public Object[] newsletterIncorrectEmail() {
 
-        String[] data = JSONReader.get("newsletter", "incorrectEmail");
-
-        return new Object[] {data};
-    }
-
-    @DataProvider(name = "newsletterEmptyEmailField")
-    public Object[] newsletterEmptyEmailField() {
-
-        String[] data = JSONReader.get("newsletter", "emptyEmailField");
-
-        return new Object[] {data};
+        return JSONReader.get("newsletter", "incorrectEmail");
     }
 
     @DataProvider(name = "newsletterCorrectEmail")
     public Object[] newsletterCorrectEmail() {
 
-        String[] data = JSONReader.get("newsletter", "correctEmail");
-
-        return new Object[] {data};
+        return JSONReader.get("newsletter", "correctEmail");
     }
 
     @DataProvider(name = "promosHomeDecor")
