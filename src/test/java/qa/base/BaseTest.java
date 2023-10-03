be84f7f591d9770f0f2a667aea94585a3ff59e0c
aksi1984 <pablo_xmen@op.pl>
2023-10-03 10:03:49 +0200

    Changed @BeforeTest to @BeforeClass, added @BeforeMethod and @After method annotations

--- src/test/java/qa/base/BaseTest.java
+++ src/test/java/qa/base/BaseTest.java
@@ -1,16 +1,26 @@
 package qa.base;
 
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 import utils.JSONReader;
 import playwright.PlaywrightLauncher;
 
 public class BaseTest {
 
-    @BeforeTest
+    @BeforeClass
+    public void init() {
+
+        JSONReader.read();
+    }
+
+    @BeforeMethod
     public void launch() {
 
         PlaywrightLauncher.launch();
+    }
 
-        JSONReader.read();
+    @AfterMethod
+    public void tearDown() {
+
+        PlaywrightLauncher.close();
     }
 }
