6c6643517fdd990c6349e0e8dae3e1f30084e8ed
aksi1984 <pablo_xmen@op.pl>
2023-10-12 18:35:07 +0200

    Removed unnecessary locators and methods

--- src/test/java/pages/components/Promos.java
+++ src/test/java/pages/components/Promos.java
@@ -1,36 +1,17 @@
 package pages.components;
 
-import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import qa.base.BasePage;
 
 public class Promos extends BasePage {
 
-    private final Locator homeAndDecor;
-    private final Locator shopPrivateSales;
-    private final Locator travelGear;
-
     public Promos(Page page) {
 
         super(page);
-
-        homeAndDecor = getPage().getByAltText("Physical & Virtual Gift Cards");
-        shopPrivateSales = getPage().getByAltText("Shop Private Sales - Members Only");
-        travelGear = getPage().getByAltText("Travel Gear for Every Occasion");
     }
 
-    public void clickHomeAndDecorImage() {
+    public void clickImage(String altText) {
 
-        homeAndDecor.click();
-    }
-
-    public void clickShopPrivateSalesImage() {
-
-        shopPrivateSales.click();
-    }
-
-    public void clickTravelGearImage() {
-
-        travelGear.click();
+        getPage().getByAltText(altText).click();
     }
 }
