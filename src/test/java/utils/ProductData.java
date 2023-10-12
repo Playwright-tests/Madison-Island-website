62d2e77688b86f56ca89584975d27dfe5476c38f
aksi1984 <pablo_xmen@op.pl>
2023-10-12 18:33:40 +0200

    Created the ProductData class

---
+++ src/test/java/utils/ProductData.java
@@ -0,0 +1,46 @@
+package utils;
+
+public class ProductData {
+
+    private String category;
+    private String productType;
+    private String name;
+    private String color;
+    private String size;
+    private String quantity;
+
+    public ProductData() { }
+    public ProductData(String category, String productType, String name, String color, String size, String quantity) {
+
+        this.category = category;
+        this.productType = productType;
+        this.name = name;
+        this.color = color;
+        this.size = size;
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+}
