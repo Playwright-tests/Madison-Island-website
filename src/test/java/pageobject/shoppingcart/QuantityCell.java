606b8edd4cba5ce196d661a48e4028a34b711d28
aksi1984 <pablo_xmen@op.pl>
2023-10-13 18:29:40 +0200

    Created the QuantityCell class

---
+++ src/test/java/pageobject/shoppingcart/QuantityCell.java
@@ -0,0 +1,35 @@
+package pageobject.shoppingcart;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import qa.base.BasePage;
+
+public class QuantityCell extends BasePage {
+
+    private final Locator quantityField;
+    private final Locator updateButton;
+
+    public QuantityCell(Page page, Locator parent) {
+
+        super(page);
+
+        quantityField = parent.getByRole(AriaRole.TEXTBOX);
+        updateButton = parent.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Update"));
+    }
+
+    public void clickQuantityField() {
+
+        quantityField.click();
+    }
+
+    public void setQuantity(String quantity) {
+
+        quantityField.fill(quantity);
+    }
+
+    public void clickUpdateButton() {
+
+        updateButton.click();;
+    }
+}
