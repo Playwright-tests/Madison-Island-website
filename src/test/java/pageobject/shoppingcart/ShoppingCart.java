f1404cad727087f46498e5d1cf8eb230a4f9aaec
aksi1984 <pablo_xmen@op.pl>
2023-10-13 18:30:19 +0200

    Created the ShoppingCart class

---
+++ src/test/java/pageobject/shoppingcart/ShoppingCart.java
@@ -0,0 +1,21 @@
+package pageobject.shoppingcart;
+
+import com.microsoft.playwright.Page;
+import qa.base.BasePage;
+
+public class ShoppingCart extends BasePage {
+
+    private final Table table;
+
+    public ShoppingCart(Page page) {
+
+        super(page);
+
+        table = new Table(page);
+    }
+
+    public Table getTable() {
+
+        return table;
+    }
+}
