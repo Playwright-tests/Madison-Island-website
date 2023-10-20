e81d122a139db3dd52d9c164857956101783b35c
aksi1984 <pablo_xmen@op.pl>
2023-10-20 17:22:51 +0200

    Removed Consumer

--- src/test/java/tests/MainMenuTest.java
+++ src/test/java/tests/MainMenuTest.java
@@ -4,11 +4,9 @@ import dataProvider.Provider;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pageobject.components.MainMenu;
 import pageobject.sections.Header;
 import qa.base.BaseTest;
 import utils.Pair;
-import java.util.function.Consumer;
 import static playwright.PlaywrightLauncher.*;
 
 public class MainMenuTest extends BaseTest {
@@ -21,42 +19,43 @@ public class MainMenuTest extends BaseTest {
         header = new Header(getPage());
     }
 
-    private void check(Consumer<MainMenu> consumer, String menuButton, String expectedURL) {
+    private void check(String button, Pair<String, String> data) {
 
-        header.getMainMenu().hoverParent(menuButton);
-        consumer.accept(header.getMainMenu());
+        header.getMainMenu().hoverParent(button);
+        header.getMainMenu().clickItem(data.getFirst());
 
-        Assert.assertEquals(getPage().url(), expectedURL);
+        Assert.assertEquals(getPage().url(), data.getSecond());
     }
 
     @Test(dataProvider = "mainMenuWomen", dataProviderClass = Provider.class)
     public void women(Pair<String, String> data) {
 
-        check((MainMenu mm)->{ mm.clickWomenItem(data.getFirst()); }, "Women", data.getSecond());
+        check("Women", data);
     }
 
     @Test(dataProvider = "mainMenuMen", dataProviderClass = Provider.class)
     public void men(Pair<String, String> data) {
 
-        check((MainMenu mm)->{ mm.clickMenItem(data.getFirst()); }, "Men", data.getSecond());
+        check("Men", data);
     }
 
     @Test(dataProvider = "mainMenuAccessories", dataProviderClass = Provider.class)
     public void accessories(Pair<String, String> data) {
 
-        check((MainMenu mm)->{ mm.clickAccessoriesItem(data.getFirst()); }, "Accessories", data.getSecond());
+        check("Accessories", data);
     }
 
     @Test(dataProvider = "mainMenuHomeDecor", dataProviderClass = Provider.class)
     void homeAndDecor(Pair<String, String> data) {
 
-        check((MainMenu mm)->{ mm.clickHomeAndDecorItem(data.getFirst()); }, "Home & Decor", data.getSecond());
+        check("Home & Decor", data);
     }
 
     @Test(dataProvider = "mainMenuSale", dataProviderClass = Provider.class)
     void sale(Pair<String, String> data) {
 
-        check((MainMenu mm)->{ mm.clickSaleItem(data.getFirst()); }, "Sale", data.getSecond());
+        check("Sale", data);
+        //check((MainMenu mm)->{ mm.clickSaleItem(data.getFirst()); }, "Sale", data.getSecond());
     }
 
     @Test(dataProvider = "mainMenuVIP", dataProviderClass = Provider.class)
