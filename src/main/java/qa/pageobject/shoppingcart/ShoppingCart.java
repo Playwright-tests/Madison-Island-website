cbc3140da3686ce6d78bbe5a623fdba95fab4ada
aksi1984 <pablo_xmen@op.pl>
2023-10-21 22:16:43 +0200

    Added Locators and methods

--- src/main/java/qa/pageobject/shoppingcart/ShoppingCart.java
+++ src/main/java/qa/pageobject/shoppingcart/ShoppingCart.java
@@ -1,21 +1,36 @@
 package qa.pageobject.shoppingcart;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import qa.base.BasePage;
 
 public class ShoppingCart extends BasePage {
 
     private final Table table;
+    private final Locator errorMessage;
+    private final Locator pageTitle;
 
     public ShoppingCart(Page page) {
 
         super(page);
 
         table = new Table(page);
+        errorMessage = page.locator("li.error-msg");
+        pageTitle = page.locator("div.page-title");
     }
 
     public Table getTable() {
 
         return table;
+    }
+
+    public boolean isErrorMessageVisible() {
+
+        return errorMessage.isVisible();
+    }
+
+    public String getPageTitleText() {
+
+        return pageTitle.textContent().trim();
     }
 }
