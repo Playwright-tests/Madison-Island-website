8e153c08a393da12ccb9dc116a2d96e6b5023375
aksi1984 <pablo_xmen@op.pl>
2023-11-05 22:50:11 +0100

    Added exception signature

--- src/main/java/qa/base/BaseTest.java
+++ src/main/java/qa/base/BaseTest.java
@@ -1,13 +1,16 @@
 package qa.base;
 
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.*;
 import qa.utils.JSONReader;
 import qa.playwright.PlaywrightLauncher;
 
+import java.io.IOException;
+
 public class BaseTest {
 
     @BeforeClass
-    public void init() {
+    public void init() throws IOException, ParseException {
 
         JSONReader.read();
     }
