45f89f41c0ac011947cd74d262fef95fdb1c8d49
aksi1984 <pablo_xmen@op.pl>
2023-11-05 09:11:53 +0100

    Created the CouponCodeForm class

---
+++ src/main/java/qa/pageobject/shoppingcart/CouponCodeForm.java
@@ -0,0 +1,50 @@
+package qa.pageobject.shoppingcart;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import qa.base.BasePage;
+
+public class CouponCodeForm extends BasePage {
+
+    private final String errorMessageSelector;
+    private final Locator couponCodeField;
+    private final Locator applyButton;
+    private final Locator errorMessage;
+
+    public CouponCodeForm(Page page) {
+
+        super(page);
+
+        errorMessageSelector = "#advice-required-entry-coupon_code";
+
+        couponCodeField = page.locator("#coupon_code");
+        applyButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Apply"));
+        errorMessage = page.locator(errorMessageSelector);
+    }
+
+    public void setCouponCode(String couponCode) {
+
+        couponCodeField.fill(couponCode);
+    }
+
+    public void clickApplyButton() {
+
+        applyButton.click();
+    }
+
+    public boolean isErrorMessageVisible() {
+
+        return errorMessage.isVisible();
+    }
+
+    public String getErrorMessageSelector() {
+
+        return errorMessageSelector;
+    }
+
+    public String getErrorMessageText() {
+
+        return errorMessage.textContent();
+    }
+}
