53be4ed4f8037cf08298b37294f1f8ca5bd24b8d
aksi1984 <pablo_xmen@op.pl>
2023-11-05 14:22:03 +0100

    Created the EstimateShippingFormFactory class

---
+++ src/main/java/qa/factories/EstimateShippingFormFactory.java
@@ -0,0 +1,29 @@
+package qa.factories;
+
+import com.microsoft.playwright.Page;
+import qa.pageobject.shoppingcart.EstimateShippingForm;
+import qa.utils.EstimateShippingData;
+
+public class EstimateShippingFormFactory {
+
+    public static EstimateShippingForm withAllFields(Page page, EstimateShippingData data) {
+
+        EstimateShippingForm estimateShippingForm = new EstimateShippingForm(page);
+
+        estimateShippingForm.selectCountry(data.getCountry());
+
+        if (data.getStateType().contains("dropdownList")) {
+
+            estimateShippingForm.selectState(data.getState());
+        }
+        else {
+
+            estimateShippingForm.setState(data.getState());
+        }
+
+        estimateShippingForm.setCity(data.getCity());
+        estimateShippingForm.setPostcode(data.getPostcode());
+
+        return estimateShippingForm;
+    }
+}
