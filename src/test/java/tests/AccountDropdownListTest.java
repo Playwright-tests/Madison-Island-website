013da0a93bc50b1794403a175092b2fc774fb0b6
aksi1984 <pablo_xmen@op.pl>
2023-11-06 10:20:03 +0100

    Added reports

--- src/test/java/tests/AccountDropdownListTest.java
+++ src/test/java/tests/AccountDropdownListTest.java
@@ -4,6 +4,7 @@ import qa.dataProvider.Provider;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import qa.extentreportsmanager.ExtentReportsManager;
 import qa.pageobject.components.AccountDropdownList;
 import qa.base.BaseTest;
 import qa.utils.Pair;
@@ -21,11 +22,15 @@ public class AccountDropdownListTest extends BaseTest {
     }
 
     @Test(dataProvider = "accountDropdownList", dataProviderClass = Provider.class)
-    void link(Pair<String, String> data) {
+    void clickingButton(Pair<String, String> data) {
+
+        ExtentReportsManager.createTest("Clicking the \"" + data.getFirst() + "\" button",
+                "Checking whether the page with the address \"" + data.getSecond() +  "\" opens after clicking the \"" + data.getFirst() + "\" button.");
 
         accountDropdownList.clickAccountNav();
         accountDropdownList.clickItem(data.getFirst());
 
-        Assert.assertEquals(getPage().url(), data.getSecond());
+        Assert.assertEquals(getPage().url(), data.getSecond(),
+                "The page with address \"" + data.getSecond() + "\" has not been opened");
     }
 }
