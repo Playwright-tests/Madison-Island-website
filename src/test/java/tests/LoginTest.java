65b04bb977ffae152a83bab49aa5cd2923f56f01
aksi1984 <pablo_xmen@op.pl>
2023-11-14 23:18:15 +0100

    Added assertions to the validationEmailField() method

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -9,8 +9,9 @@ import qa.pageobject.components.AccountDropdownList;
 import qa.pageobject.components.LoginForm;
 import qa.playwright.PlaywrightLauncher;
 import qa.base.BaseTest;
-import qa.utils.Pair;
-import java.util.List;
+import qa.utils.Credentials;
+import qa.utils.NewsletterData;
+
 
 public class LoginTest extends BaseTest {
 
@@ -26,47 +27,57 @@ public class LoginTest extends BaseTest {
         accountDropdownList.clickItem("My Account");
     }
 
-    private void setData(String email, String password) {
+    private void setData(Credentials credentials) {
 
-        loginForm.setEmail(email);
-        loginForm.setPassword(password);
+        loginForm.setEmail(credentials.getEmail());
+        loginForm.setPassword(credentials.getPassword());
         loginForm.clickLoginButton();
     }
 
-    @Test(dataProvider = "incorrectEmail", dataProviderClass = Provider.class)
-    public void incorrectEmail(List<Pair<String, String>> data) {
+    @Test(dataProvider = "validationEmailField", dataProviderClass = Provider.class)
+    public void validationEmailField(NewsletterData newsletterData) {
 
+        ExtentReportsManager.createTest("\"" + newsletterData.getEmail() + "\" as an incorrect email",
+                "Checking whether a message about incorrect email format is displayed");
+
+        setData(new Credentials(newsletterData));
+
+        Assert.assertNotEquals(loginForm.getValidationMessage(), "",
+                "No validation message");
+
+        Assert.assertEquals(loginForm.getValidationMessage(), newsletterData.getValidationMessage(),
+                "Incorrect message content");
     }
 
     @Test(dataProvider = "emptyEmailField", dataProviderClass = Provider.class)
-    public void blankEmailField(Pair<String, String> data) {
+    public void emptyEmailField(Credentials credentials) {
 
         ExtentReportsManager.createTest("Blank \"Email Address\" field",
-                "Checking whether a message about an blank \"Email Address\" field is displayed");
+                "Checking whether a message about an empty \"Email Address\" field is displayed");
 
-        setData(data.getFirst(), data.getSecond());
+        setData(credentials);
         Assert.assertTrue(loginForm.isRequiredEmailMessageVisible(),
                 "The message about blank \"Email Address\" has not been displayed");
     }
 
     @Test(dataProvider = "incorrectPassword", dataProviderClass = Provider.class)
-    public void incorrectPassword(Pair<String, String> data) {
+    public void incorrectPassword(Credentials credentials) {
 
-        ExtentReportsManager.createTest("Logging in using \"" + data.getSecond() + "\" as incorrect password",
+        ExtentReportsManager.createTest("Logging in using \"" + credentials.getPassword() + "\" as incorrect password",
                 "Checking whether a message about an incorrect password is displayed");
 
-        setData(data.getFirst(), data.getSecond());
+        setData(credentials);
         Assert.assertTrue(loginForm.isInvalidLoginOrPasswordMessageVisible(),
                 "The message about an incorrect password has not been displayed");
     }
 
     @Test(dataProvider = "emptyPasswordField", dataProviderClass = Provider.class)
-    public void blankPasswordField(Pair<String, String> data) {
+    public void blankPasswordField(Credentials credentials) {
 
         ExtentReportsManager.createTest("Blank \"Password\" field",
                 "Checking whether a message about an empty \"Password\" field is displayed");
 
-        setData(data.getFirst(), data.getSecond());
+        setData(credentials);
         Assert.assertTrue(loginForm.isRequiredPasswordMessageVisible(),
                 "The message about blank \"Password\" has not been displayed");
     }
