686db0f7ab1398c5fc1a8d8d84616f9b41abb55c
aksi1984 <pablo_xmen@op.pl>
2023-11-22 08:17:27 +0100

    Added fields and methods

--- src/main/java/qa/playwright/PlaywrightBrowserLauncher.java
+++ src/main/java/qa/playwright/PlaywrightBrowserLauncher.java
@@ -1,7 +1,44 @@
 package qa.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
-abstract class PlaywrightBrowserLauncher {
-    public abstract Browser launch();
+public abstract class PlaywrightBrowserLauncher {
+
+    protected Playwright playwright;
+    protected Browser browser;
+    protected static Page page;
+
+    protected void setPlaywright() {
+
+        playwright = Playwright.create();
+    }
+
+    protected void setPage(String url) {
+
+        page = browser.newPage();
+        page.navigate(url);
+    }
+    protected abstract void setBrowser();
+
+
+    public void launch(String url) {
+
+        setPlaywright();
+        setBrowser();
+        setPage(url);
+    }
+
+    public static Page getPage() {
+
+        return page;
+    }
+
+    public void close() {
+
+        page.close();
+        browser.close();
+        playwright.close();
+    }
 }
