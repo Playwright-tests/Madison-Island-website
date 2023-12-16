1d7b8b60934a1048e89b79b0f4f284f648c1b55e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-16 18:43:40 +0100

    Renamed and changed code

---
+++ src/main/java/qa/helpers/ProductShopHandler.java
@@ -0,0 +1,36 @@
+package qa.helpers;
+
+import com.microsoft.playwright.Page;
+import qa.enums.ProductShopMethods;
+import qa.pageobject.productpage.ProductShop;
+import qa.records.ProductData;
+import qa.utils.MethodsRetriever;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ProductShopHandler {
+
+    private static String[] toArray(ProductData data) {
+
+        return new String[]{ data.color(), data.quantity(), data.size() };
+    }
+
+    public static ProductShop set(Page page, ProductData data, ProductShopMethods omitted) throws InvocationTargetException, IllegalAccessException {
+
+        ProductShop productShop = new ProductShop(page);
+        MethodsRetriever<ProductShop> retriever = new MethodsRetriever<>(ProductShop.class);
+        Method[] methods = retriever.getMethods();
+        String[] clothingData = toArray(data);
+
+        for (int i = 0; i < methods.length; i++) {
+
+            if (!methods[i].getName().toLowerCase().contains(omitted.getName().toLowerCase())) {
+
+                methods[i].invoke(productShop, clothingData[i]);
+            }
+        }
+
+        return productShop;
+    }
+}
