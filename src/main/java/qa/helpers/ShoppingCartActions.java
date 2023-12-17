b40437ce5c7f9b964552f2b31e650be4b46a88bb
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-17 16:45:44 +0100

    The commented code has been removed

--- src/main/java/qa/helpers/ShoppingCartActions.java
+++ src/main/java/qa/helpers/ShoppingCartActions.java
@@ -17,19 +17,5 @@ public class ShoppingCartActions {
         ProductData[] productData = (ProductData[]) provider.correctProductData();
         ProductShop productShop = ProductShopHandler.set(page, productData[0], ProductShopMethods.ALL);
         productShop.clickAddToCartButton();
-        /*MainMenu mainMenu = new MainMenu(page);
-
-        mainMenu.hoverParent(parent);
-        mainMenu.clickItem(item);
-
-        ProductThumbnail productThumbnail = new ProductThumbnail(page, title);
-        productThumbnail.clickViewDetailsButton();
-
-        /*ProductPage productPage = new ProductPage(page);
-        productPage.setProductShop(new ProductShop(page));
-        productPage.getProductShop()
-                .setColor(color)
-                .setSize(size)
-                .clickAddToCartButton();*/
     }
 }
