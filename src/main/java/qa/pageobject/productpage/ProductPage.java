0d36e5ae76cb66ac1b5953b3ea75198737357f80
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-17 14:51:29 +0100

    Added locator and method

--- src/main/java/qa/pageobject/productpage/ProductPage.java
+++ src/main/java/qa/pageobject/productpage/ProductPage.java
@@ -1,22 +1,35 @@
 package qa.pageobject.productpage;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import qa.base.BasePage;
 
 public class ProductPage extends BasePage {
 
+    private final Locator errorMessage;
     private ProductShop productShop;
 
     public ProductPage(Page page) {
 
         super(page);
 
+        errorMessage = page.locator(".error-msg");
         this.productShop = null;
     }
 
     public void setProductShop(ProductShop productShop) {
 
         this.productShop = productShop;
+    }
+
+    public boolean isErrorMessageVisible() {
+
+        return errorMessage.isVisible();
+    }
+
+    public String getErrorMessage() {
+
+        return errorMessage.textContent();
     }
 
     public ProductShop getProductShop() {
