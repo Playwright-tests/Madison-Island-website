f4f05fd578fdf8df5f4f260386be7efb70202f0e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-19 06:38:40 +0100

    Moved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,26 @@
 package org.example;
 
-import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.AriaRole;
-
-import java.util.Locale;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
 
+        Foo foo = new Foo();
+        Method[] methods = Arrays.stream(Foo.class.getMethods()).filter(m -> m.getName().contains("fun")).toArray(Method[]::new);
+
+        String omitted = "fun2";
+        String[] data = { "For fun1 !!!", "For fun2 !!!", "For fun3 !!!" };
+
+        for (int i = 0; i < methods.length; i++) {
+
+            if (!methods[i].getName().contains(omitted)) {
+                methods[i].invoke(foo, data[i]);
+            }
+        }
     }
 }
