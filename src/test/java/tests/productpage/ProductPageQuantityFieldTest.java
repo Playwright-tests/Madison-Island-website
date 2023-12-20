fb9b95a0372f8702ab2cf6ad1d1cc023f5f61e4c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-20 08:49:56 +0100

    Added reports

--- src/test/java/tests/productpage/ProductPageQuantityFieldTest.java
+++ src/test/java/tests/productpage/ProductPageQuantityFieldTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.dataProvider.Provider;
 import qa.enums.ProductShopMethods;
+import qa.extentreportsmanager.ExtentReportsManager;
 import qa.helpers.ProductShopHandler;
 import qa.pageobject.productpage.ProductPage;
 import qa.records.ProductData;
@@ -32,72 +33,111 @@ public class ProductPageQuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_min", dataProviderClass = Provider.class)
     public void min(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the minimum value",
+                "Checking the system's behavior after entering the maximum value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertEquals(getPage().url(), URLs.SHOPPING_CART.getName());
+        Assert.assertEquals(getPage().url(), URLs.SHOPPING_CART.getName(),
+                "The product has not been added to the shopping cart");
     }
 
     @Test(dataProvider = "QF_aboveMin", dataProviderClass = Provider.class)
     public void aboveMin(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the above minimum value",
+                "Checking the system's behavior after entering the maximum value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertEquals(getPage().url(), URLs.SHOPPING_CART.getName());
+        Assert.assertEquals(getPage().url(), URLs.SHOPPING_CART.getName(),
+                "The product has not been added to the shopping cart");
     }
 
     @Test(dataProvider = "QF_belowMax", dataProviderClass = Provider.class)
     public void belowMax(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the below maximum value",
+                "Checking the system's behavior after entering the maximum value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible());
-        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()));
+        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible(),
+                "No error message");
+        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()),
+                "Incorrect error message content");
     }
 
     @Test(dataProvider = "QF_max", dataProviderClass = Provider.class)
     public void max(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the maximum value",
+                "Checking the system's behavior after entering the maximum value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible());
-        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()));
+        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible(),
+                "No error message");
+        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()),
+                "Incorrect error message content");
     }
 
     @Test(dataProvider = "QF_belowMin", dataProviderClass = Provider.class)
     public void belowMin(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as below minimum value",
+                "Checking the system's behavior after entering too large value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible());
+        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible(),
+                "");
         Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()));
     }
 
     @Test(dataProvider = "QF_aboveMax", dataProviderClass = Provider.class)
     public void aboveMax(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as too large value",
+                "Checking the system's behavior after entering too large value in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible());
-        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()));
+        Assert.assertTrue(productPage.getErrorMessageLocator().isVisible(),
+                "No error message");
+        Assert.assertTrue(productPage.getErrorMessage().contains(data.getMessage()),
+                "Incorrect error message content");
     }
 
     @Test(dataProvider = "QF_floatingPoint", dataProviderClass = Provider.class)
     public void floatingPoint(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the floating point",
+                "Checking the system's behavior after entering the floating point in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertNotEquals(getPage().url(), URLs.SHOPPING_CART.getName());
-        Assert.assertFalse(productPage.getProductShop().getQuantityValidationMessage().isEmpty());
-        Assert.assertEquals(productPage.getProductShop().getQuantityValidationMessage(), data.getValidationMessage());
+        Assert.assertNotEquals(getPage().url(), URLs.SHOPPING_CART.getName(),
+                "The product has been added to the shopping cart");
+        Assert.assertFalse(productPage.getProductShop().getQuantityValidationMessage().isEmpty(),
+                "No validation message");
+        Assert.assertEquals(productPage.getProductShop().getQuantityValidationMessage(), data.getValidationMessage(),
+                "Incorrect validation message");
     }
 
     @Test(dataProvider = "QF_notANumber", dataProviderClass = Provider.class)
     public void notANumber(ProductData data) throws InvocationTargetException, IllegalAccessException {
 
+        ExtentReportsManager.createTest("Setting the product quantity using \"" + data.getQuantity() + "\" as the value which is not a number",
+                "Checking the system's behavior after entering the value which is not a number in the \"QTY\" field");
+
         fill(data);
 
-        Assert.assertNotEquals(getPage().url(), URLs.SHOPPING_CART.getName());
-        Assert.assertFalse(productPage.getProductShop().getQuantityValidationMessage().isEmpty());
-        Assert.assertEquals(productPage.getProductShop().getQuantityValidationMessage(), data.getValidationMessage());
+        Assert.assertNotEquals(getPage().url(), URLs.SHOPPING_CART.getName(),
+                "The product has been added to the shopping cart");
+        Assert.assertFalse(productPage.getProductShop().getQuantityValidationMessage().isEmpty(),
+                "No validation message");
+        Assert.assertEquals(productPage.getProductShop().getQuantityValidationMessage(), data.getValidationMessage(),
+                "Incorrect validation message content");
     }
 }
