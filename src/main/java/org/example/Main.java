1650b12cdef9deabab344e873a3487ee0f456b03
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-20 07:59:20 +0100

    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,7 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args)  {
 
-        Foo foo = new Foo();
-        Method[] methods = Arrays.stream(Foo.class.getMethods()).filter(m -> m.getName().contains("fun")).toArray(Method[]::new);
-
-        String omitted = "fun2";
-        String[] data = { "For fun1 !!!", "For fun2 !!!", "For fun3 !!!" };
-
-        for (int i = 0; i < methods.length; i++) {
-
-            if (!methods[i].getName().contains(omitted)) {
-                methods[i].invoke(foo, data[i]);
-            }
-        }
     }
 }
