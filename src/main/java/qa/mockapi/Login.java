9bdccbe1f2a159aaddffb144f738ef5d5f64f700
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 18:17:13 +0100

    Created the class

---
+++ src/main/java/qa/mockapi/Login.java
@@ -0,0 +1,11 @@
+package qa.mockapi;
+
+public class Login {
+
+    public static final String CORRECT = "MI_LoginCorrect";
+    public static final String INCORRECT_EMAIL = "MI_LoginIncorrectEmail";
+    public static final String INCORRECT_PASSWORD = "MI_LoginIncorrectPassword";
+    public static final String INCORRECT_EMAIL_FORMAT = "MI_LoginIncorrectEmailFormat";
+    public static String BLANK_EMAIL_FIELD = "MI_LoginBlankEmailField";
+    public static String BLANK_PASSWORD_FIELD = "MI_LoginBlankPasswordField";
+}
