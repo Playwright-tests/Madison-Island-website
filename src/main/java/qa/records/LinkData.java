89335308a577daa2959bd7f503ef172d72749958
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 21:11:57 +0100

    Made the constructor default

--- src/main/java/qa/records/LinkData.java
+++ src/main/java/qa/records/LinkData.java
@@ -5,11 +5,7 @@ import lombok.Getter;
 @Getter
 public class LinkData {
 
-    private final String link;
-    private final String url;
-    public LinkData(String link, String url) {
-
-        this.link = link;
-        this.url = url;
-    }
+    private String link;
+    private String url;
+    public LinkData() { }
 }
