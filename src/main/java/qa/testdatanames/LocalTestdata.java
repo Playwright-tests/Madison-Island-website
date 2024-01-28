ff928fbea0194be5b163f64f432d914b86cfa3f9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 22:34:04 +0100

    Created the class

---
+++ src/main/java/qa/testdatanames/LocalTestdata.java
@@ -0,0 +1,18 @@
+package qa.testdatanames;
+
+public class LocalTestdata {
+
+    public static final String LINKS = "links";
+    public static final String MAIN_MENU_WOMEN = "mainMenuWomen";
+    public static final String MAIN_MENU_MEN = "mainMenuMen";
+    public static final String MAIN_MENU_ACCESSORIES = "mainMenuAccessories";
+    public static final String MAIN_MENU_HOME_DECOR = "mainMenuHomeDecor";
+    public static final String MAIN_MENU_SALE = "mainMenuSale";
+    public static final String MAIN_MENU_VIP = "mainMenuVIP";
+    public static final String ACCOUNT_DROPDOWN_LIST = "accountDropdownList";
+    public static final String FOOTER_COMPANY = "footerCompany";
+    public static final String FOOTER_QUICK_LINKS = "footerQuickLinks";
+    public static final String FOOTER_ACCOUNT = "footerAccount";
+    public static final String FOOTER_CONNECT_WITH_US = "footerConnectWithUs";
+    public static final String PROMOS = "promos";
+}
