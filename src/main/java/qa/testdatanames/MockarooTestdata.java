00b98a4cd9fe090d98cd196948231cb1229f2d7a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 22:34:27 +0100

    Created the class

---
+++ src/main/java/qa/testdatanames/MockarooTestdata.java
@@ -0,0 +1,7 @@
+package qa.testdatanames;
+
+public class MockarooTestdata {
+
+    public static final String NEWSLETTER_CORRECT_EMAIL = "newsletterCorrectEmail";
+    public static final String NEWSLETTER_INCORRECT_EMAIL_FORMAT = "newsletterIncorrectEmailFormat";
+}
