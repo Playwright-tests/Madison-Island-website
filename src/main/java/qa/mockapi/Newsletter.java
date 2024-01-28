9a500285664ae96d0950ad21c416daf71d74ab45
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 22:16:49 +0100

    Created the class

---
+++ src/main/java/qa/mockapi/Newsletter.java
@@ -0,0 +1,7 @@
+package qa.mockapi;
+
+public class Newsletter {
+
+    public static final String CORRECT = "MI_NewsletterCorrect";
+    public static final String INCORRECT_EMAIL_FORMAT = "MI_NewsletterIncorrectEmailFormat";
+}
