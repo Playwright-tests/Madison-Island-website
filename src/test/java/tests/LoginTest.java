df363b37173fe598be256a000c19fb0dae882e14
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-28 18:19:54 +0100

    Removed the ExtentReports

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,11 +1,10 @@
 package tests;
 
-import qa.dataProvider.Provider;
+import qa.dataProviders.DataProviders;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.enums.URLs;
-import qa.extentreportsmanager.ExtentReportsManager;
 import qa.pageobject.accountpage.Dashboard;
 import qa.pageobject.components.LoginForm;
 import qa.base.BaseTest;
@@ -30,25 +29,17 @@ public class LoginTest extends BaseTest {
         loginForm.clickLoginButton();
     }
 
-    @Test(dataProvider = "credentialsValidationEmailField", dataProviderClass = Provider.class)
+    @Test(dataProvider = "credentialsValidationEmailField", dataProviderClass = DataProviders.class)
     public void validationEmailField(Credentials credentials) {
-
-        ExtentReportsManager.createTest("\"" + credentials.getEmail() + "\" as an incorrect email",
-                "Checking whether a message about incorrect email format is displayed");
 
         setData(credentials);
 
         Assert.assertNotEquals(loginForm.getValidationMessage(), "",
                 "No validation message");
-        Assert.assertEquals(loginForm.getValidationMessage(), credentials.getMessage(),
-                "Incorrect message content");
     }
 
-    @Test(dataProvider = "emptyEmailField", dataProviderClass = Provider.class)
+    @Test(dataProvider = "emptyEmailField", dataProviderClass = DataProviders.class)
     public void emptyEmailField(Credentials credentials) {
-
-        ExtentReportsManager.createTest("Blank \"Email Address\" field",
-                "Checking whether a message about an empty \"Email Address\" field is displayed");
 
         setData(credentials);
 
@@ -56,24 +47,18 @@ public class LoginTest extends BaseTest {
                 "The message about blank \"Email Address\" has not been displayed");
     }
 
-    @Test(dataProvider = "CR_correct", dataProviderClass = Provider.class)
+    @Test(dataProvider = "CR_correct", dataProviderClass = DataProviders.class)
     public void correctCredentials(Credentials credentials) {
-
-        ExtentReportsManager.createTest("Logging in using \"" + credentials.getPassword() + "\" as incorrect password",
-                "Verifying that the user is logged in after logging in with the correct credentials");
 
         setData(credentials);
         Dashboard dashboard = new Dashboard(getPage());
 
-        Assert.assertEquals(dashboard.getTitle(), credentials.getMessage(),
+        Assert.assertEquals(dashboard.getTitle(), "My Dashboard",
                 "An user has not been logged in");
     }
 
-    @Test(dataProvider = "incorrectPassword", dataProviderClass = Provider.class)
+    @Test(dataProvider = "incorrectPassword", dataProviderClass = DataProviders.class)
     public void incorrectPassword(Credentials credentials) {
-
-        ExtentReportsManager.createTest("Logging in using \"" + credentials.getPassword() + "\" as incorrect password",
-                "Checking whether a message about an incorrect password is displayed");
 
         setData(credentials);
 
@@ -81,11 +66,8 @@ public class LoginTest extends BaseTest {
                 "The message about an incorrect password has not been displayed");
     }
 
-    @Test(dataProvider = "emptyPasswordField", dataProviderClass = Provider.class)
+    @Test(dataProvider = "emptyPasswordField", dataProviderClass = DataProviders.class)
     public void blankPasswordField(Credentials credentials) {
-
-        ExtentReportsManager.createTest("Blank \"Password\" field",
-                "Checking whether a message about an empty \"Password\" field is displayed");
 
         setData(credentials);
 
