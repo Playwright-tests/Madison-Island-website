0edb485193129aa165ed4d6a7611a47e6ffe39b8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-29 22:41:58 +0100

    Removed

--- src/main/java/qa/support/jsonfilenames/Newsletter.java
+++
@@ -1,7 +0,0 @@
-package qa.support.jsonfilenames;
-
-public class Newsletter {
-
-    public static final String CORRECT = "MI_NewsletterCorrect";
-    public static final String INCORRECT_EMAIL_FORMAT = "MI_NewsletterIncorrectEmailFormat";
-}
