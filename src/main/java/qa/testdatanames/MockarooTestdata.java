79313ed4c8a4d034a5af06945078c30ea0dd5920
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-29 08:11:22 +0100

    Removed

--- src/main/java/qa/testdatanames/MockarooTestdata.java
+++
@@ -1,7 +0,0 @@
-package qa.testdatanames;
-
-public class MockarooTestdata {
-
-    public static final String NEWSLETTER_CORRECT_EMAIL = "newsletterCorrectEmail";
-    public static final String NEWSLETTER_INCORRECT_EMAIL_FORMAT = "newsletterIncorrectEmailFormat";
-}
