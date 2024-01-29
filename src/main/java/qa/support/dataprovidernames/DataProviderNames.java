d6a0a8117e0241d4628192a485701e85a08cb892
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-29 23:11:23 +0100

    Changed string names

--- src/main/java/qa/support/dataprovidernames/DataProviderNames.java
+++ src/main/java/qa/support/dataprovidernames/DataProviderNames.java
@@ -3,18 +3,26 @@ package qa.support.dataprovidernames;
 public class DataProviderNames {
 
     public static final String LINKS = "links";
-    public static final String MAIN_MENU_WOMEN = "mainMenuWomen";
-    public static final String MAIN_MENU_MEN = "mainMenuMen";
-    public static final String MAIN_MENU_ACCESSORIES = "mainMenuAccessories";
-    public static final String MAIN_MENU_HOME_DECOR = "mainMenuHomeDecor";
-    public static final String MAIN_MENU_SALE = "mainMenuSale";
-    public static final String MAIN_MENU_VIP = "mainMenuVIP";
+    public static final String WOMEN = "women";
+    public static final String MEN = "men";
+    public static final String ACCESSORIES = "accessories";
+    public static final String HOME_DECOR = "homeDecor";
+    public static final String SALE = "sale";
+    public static final String VIP = "VIP";
+
+    public static final String CORRECT = "correct";
+    public static final String INCORRECT_EMAIL = "incorrectEmail";
+    public static final String INCORRECT_PASSWORD = "incorrectPassword";
+    public static final String BLANK_EMAIL_FIELD = "blankEmailField";
+    public static final String BLANK_PASSWORD_FIELD = "blankPasswordField";
+    public static final String INCORRECT_EMAIL_FORMAT = "incorrectEmailFormat";
     public static final String ACCOUNT_DROPDOWN_LIST = "accountDropdownList";
-    public static final String FOOTER_COMPANY = "footerCompany";
-    public static final String FOOTER_QUICK_LINKS = "footerQuickLinks";
-    public static final String FOOTER_ACCOUNT = "footerAccount";
-    public static final String FOOTER_CONNECT_WITH_US = "footerConnectWithUs";
+    public static final String COMPANY = "company";
+    public static final String QUICK_LINKS = "quickLinks";
+    public static final String ACCOUNT = "account";
+    public static final String CONNECT_WITH_US = "connectWithUs";
     public static final String PROMOS = "promos";
-    public static final String NEWSLETTER_CORRECT_EMAIL = "newsletterCorrectEmail";
-    public static final String NEWSLETTER_INCORRECT_EMAIL_FORMAT = "newsletterIncorrectEmailFormat";
+    public static final String LOWER_UPPER = "lowerUpper";
+    public static final String INCORRECT = "incorrect";
+    public static final String NAUGHTY_STRINGS = "naughtyString";
 }
