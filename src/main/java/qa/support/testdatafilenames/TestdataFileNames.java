8dc6634849587ba3c5b5aee1c62f07a9ab7d5914
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-29 23:24:09 +0100

    Added field

--- src/main/java/qa/support/testdatafilenames/TestdataFileNames.java
+++ src/main/java/qa/support/testdatafilenames/TestdataFileNames.java
@@ -2,6 +2,7 @@ package qa.support.testdatafilenames;
 
 public class TestdataFileNames {
 
+    public static final String LINKS = "links";
     public static final String CREDENTIALS = "MI_Credentials";
     public static final String NEWSLETTER = "MI_Newsletter";
     public static final String PHRASES = "MI_Phrases";
