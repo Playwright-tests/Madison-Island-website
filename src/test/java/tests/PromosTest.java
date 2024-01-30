076d0c147c76b15a30e57a89da551a51e75a9ad4
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-30 08:18:44 +0100

    Added @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/PromosTest.java
+++ src/test/java/tests/PromosTest.java
@@ -1,17 +1,38 @@
 package tests;
 
-import qa.dataProviders.DataProviders;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import qa.dataProviders.PromosDataProvider;
+import qa.enums.DataDownloadMode;
+import qa.exceptions.MockarooRequestException;
 import qa.support.dataprovidernames.DataProviderNames;
 import qa.pageobject.components.Promos;
 import qa.base.BaseTest;
 import qa.records.LinkData;
+import qa.support.testdatafilenames.TestdataFileNames;
+
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class PromosTest extends BaseTest {
 
     private Promos promos;
+
+    @BeforeSuite
+    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
+
+        PromosDataProvider.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
+    }
+
+    @AfterSuite
+    public void clearTestdata() {
+
+        PromosDataProvider.clear();
+    }
 
     @BeforeMethod
     public void create() {
@@ -19,7 +40,7 @@ public class PromosTest extends BaseTest {
         promos = new Promos(getPage());
     }
 
-    @Test(dataProvider = DataProviderNames.PROMOS, dataProviderClass = DataProviders.class)
+    @Test(dataProvider = DataProviderNames.PROMOS, dataProviderClass = PromosDataProvider.class)
     public void images(LinkData linkData) {
 
         promos.clickImage(linkData.getLink());
