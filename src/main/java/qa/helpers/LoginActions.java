2025797ba5c2ccf3fd6e36e08a1ffb97b1f95542
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-30 12:18:00 +0100

    Added loading the testdata

--- src/main/java/qa/helpers/LoginActions.java
+++ src/main/java/qa/helpers/LoginActions.java
@@ -2,15 +2,20 @@ package qa.helpers;
 
 import com.microsoft.playwright.Page;
 import qa.dataProviders.CredentialsDataProviders;
+import qa.enums.DataDownloadMode;
+import qa.exceptions.MockarooRequestException;
 import qa.pageobject.components.LoginForm;
 import qa.records.Credentials;
+import qa.support.testdatafilenames.TestdataFileNames;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class LoginActions {
 
-    public static void login(Page page) throws IOException {
+    public static void login(Page page) throws IOException, URISyntaxException, MockarooRequestException {
 
+        CredentialsDataProviders.loadTestdata(TestdataFileNames.CREDENTIALS, DataDownloadMode.LOCAL);
         CredentialsDataProviders dataProviders = new CredentialsDataProviders();
         Credentials[] credentials = (Credentials[]) dataProviders.correct();
 
