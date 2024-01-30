41b509bc797a68b3e0950b3742d97497f7a0d99c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-30 12:14:41 +0100

    Added @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/account/SideMenuTest.java
+++ src/test/java/tests/account/SideMenuTest.java
@@ -1,22 +1,45 @@
 package tests.account;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.dataProvider.Provider;
+import qa.dataProviders.DataProviders;
+import qa.dataProviders.SideMenuDataProviders;
+import qa.enums.DataDownloadMode;
 import qa.enums.URLs;
-import qa.extentreportsmanager.ExtentReportsManager;
+import qa.exceptions.MockarooRequestException;
 import qa.helpers.LoginActions;
 import qa.pageobject.accountpage.AccountPage;
 import qa.records.LinkData;
+import qa.support.dataprovidernames.DataProviderNames;
+import qa.support.testdatafilenames.TestdataFileNames;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class SideMenuTest extends BaseTest {
 
     private AccountPage accountPage;
 
+    @BeforeSuite
+    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
+
+        SideMenuDataProviders.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
+    }
+
+    @AfterSuite
+    public void clearTestdata() {
+
+        SideMenuDataProviders.clear();
+    }
+
     @BeforeMethod
-    public void create() {
+    public void create() throws IOException, URISyntaxException, MockarooRequestException {
 
         goToPage(URLs.LOGIN_PAGE.getName());
         LoginActions.login(getPage());
@@ -24,11 +47,8 @@ public class SideMenuTest extends BaseTest {
         accountPage = new AccountPage(getPage());
     }
 
-    @Test(dataProvider = "sideMenu", dataProviderClass = Provider.class)
+    @Test(dataProvider = DataProviderNames.SIDE_MENU, dataProviderClass = SideMenuDataProviders.class)
     public void links(LinkData linkData) {
-
-        ExtentReportsManager.createTest("Clicking the \"" + linkData.getLink() + "\" button",
-                "Checking whether the page with the address \"" + linkData.getUrl() +  "\" opens after clicking the \"" + linkData.getLink() + "\" button.");
 
         accountPage.getSideMenu().clickLink(linkData.getLink());
 
