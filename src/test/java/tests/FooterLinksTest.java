dc2811da6b82c8a126b76d8cbc0aa595fc714e91
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-31 18:17:01 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/FooterLinksTest.java
+++ src/test/java/tests/FooterLinksTest.java
@@ -1,39 +1,17 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.dataProviders.FooterDataProviders;
-import qa.enums.DataDownloadMode;
-import qa.exceptions.MockarooRequestException;
 import qa.support.dataprovidernames.DataProviderNames;
 import qa.pageobject.sections.Footer;
 import qa.base.BaseTest;
 import qa.records.LinkData;
-import qa.support.testdatafilenames.TestdataFileNames;
-
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-
 
 public class FooterLinksTest extends BaseTest {
 
     private Footer footer;
-
-    @BeforeSuite
-    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
-
-        FooterDataProviders.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        FooterDataProviders.clear();
-    }
 
     @BeforeMethod
     public void create() {
@@ -49,25 +27,25 @@ public class FooterLinksTest extends BaseTest {
     }
 
     @Test(dataProvider = DataProviderNames.COMPANY, dataProviderClass = FooterDataProviders.class)
-    public void companySection(LinkData linkData) {
+    public void company(LinkData linkData) {
 
         check(linkData);
     }
 
     @Test(dataProvider = DataProviderNames.QUICK_LINKS, dataProviderClass = FooterDataProviders.class)
-    public void quickLinksSection(LinkData linkData) {
+    public void quickLinks(LinkData linkData) {
 
         check(linkData);
     }
 
     @Test(dataProvider = DataProviderNames.ACCOUNT, dataProviderClass = FooterDataProviders.class)
-    public void accountSection(LinkData linkData) {
+    public void account(LinkData linkData) {
 
         check(linkData);
     }
 
     @Test(dataProvider = DataProviderNames.CONNECT_WITH_US, dataProviderClass = FooterDataProviders.class)
-    public void connectWithUsSection(LinkData linkData) {
+    public void connectWithUs(LinkData linkData) {
 
         check(linkData);
     }
