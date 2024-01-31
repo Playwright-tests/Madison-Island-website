8e9b24b52cf8c359f3a95f8fce0679a0d1e70f37
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-31 18:11:17 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/AccountDropdownListTest.java
+++ src/test/java/tests/AccountDropdownListTest.java
@@ -1,39 +1,17 @@
 package tests;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import qa.dataProviders.AccountDropdownListDataProviders;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import qa.enums.DataDownloadMode;
-import qa.exceptions.MockarooRequestException;
 import qa.support.dataprovidernames.DataProviderNames;
 import qa.pageobject.components.AccountDropdownList;
 import qa.base.BaseTest;
 import qa.records.LinkData;
-import qa.support.testdatafilenames.TestdataFileNames;
-
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-
 
 public class AccountDropdownListTest extends BaseTest {
 
     private AccountDropdownList accountDropdownList;
-
-    @BeforeSuite
-    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
-
-        AccountDropdownListDataProviders.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        AccountDropdownListDataProviders.clear();
-    }
 
     @BeforeMethod
     public void create() {
