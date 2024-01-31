59e42324859707e1051b4f4599bf546a495cbfeb
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-31 20:40:04 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/SearchEngineTest.java
+++ src/test/java/tests/SearchEngineTest.java
@@ -1,40 +1,19 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.dataProviders.SearchEngineDataProviders;
-import qa.enums.DataDownloadMode;
 import qa.enums.URLs;
-import qa.exceptions.MockarooRequestException;
 import qa.pageobject.components.SearchResults;
 import qa.pageobject.sections.Header;
 import qa.base.BaseTest;
 import qa.support.dataprovidernames.DataProviderNames;
-import qa.support.testdatafilenames.TestdataFileNames;
-
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 public class SearchEngineTest extends BaseTest {
 
     private Header header;
     private SearchResults searchResults;
-
-    @BeforeSuite
-    public void loadTestdata() throws FileNotFoundException, MalformedURLException, URISyntaxException, MockarooRequestException {
-
-        SearchEngineDataProviders.loadTestdata(TestdataFileNames.PHRASES, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        SearchEngineDataProviders.clear();
-    }
 
     @BeforeMethod
     public void create() {
