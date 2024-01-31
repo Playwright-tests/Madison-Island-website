8926fde2c25dde54c88130d83d885e3f84792ab1
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-31 18:28:25 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/PromosTest.java
+++ src/test/java/tests/PromosTest.java
@@ -1,38 +1,17 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.dataProviders.PromosDataProvider;
-import qa.enums.DataDownloadMode;
-import qa.exceptions.MockarooRequestException;
 import qa.support.dataprovidernames.DataProviderNames;
 import qa.pageobject.components.Promos;
 import qa.base.BaseTest;
 import qa.records.LinkData;
-import qa.support.testdatafilenames.TestdataFileNames;
-
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 public class PromosTest extends BaseTest {
 
     private Promos promos;
-
-    @BeforeSuite
-    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
-
-        PromosDataProvider.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        PromosDataProvider.clear();
-    }
 
     @BeforeMethod
     public void create() {
