c87ca191a7c3f694dcf62e0b54abaa9ae37b5a8b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-31 18:06:13 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/MainMenuTest.java
+++ src/test/java/tests/MainMenuTest.java
@@ -1,39 +1,17 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.dataProviders.MainMenuDataProviders;
-import qa.enums.DataDownloadMode;
-import qa.exceptions.MockarooRequestException;
 import qa.support.dataprovidernames.DataProviderNames;
 import qa.pageobject.sections.Header;
 import qa.base.BaseTest;
 import qa.records.LinkData;
-import qa.support.testdatafilenames.TestdataFileNames;
-
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-
 
 public class MainMenuTest extends BaseTest {
 
     private Header header;
-
-    @BeforeSuite
-    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
-
-        MainMenuDataProviders.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        MainMenuDataProviders.clear();
-    }
 
     @BeforeMethod
     public void create() {
