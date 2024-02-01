35d51d7d2ea15e7843e739a8f7a1fb2e48b37de3
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-01 08:48:41 +0100

    Added methods

--- src/main/java/qa/json/TestdataLoader.java
+++ src/main/java/qa/json/TestdataLoader.java
@@ -1,77 +1,73 @@
 package qa.json;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import qa.IO.LocalTestdataProvider;
-import qa.IO.Service;
-import qa.testdatanames.LocalTestdata;
 import qa.records.Credentials;
 import qa.records.LinkData;
 import qa.records.NewsletterData;
+import qa.records.QuantityData;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.stream.IntStream;
 
 public class TestdataLoader {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    public static Credentials getCredentials(String name) throws IOException {
+    public static Credentials[] getCredentials(String json, String key) throws FileNotFoundException {
 
-        String data = Service.fetchTestdata(name);
-        JSONObject jsonObject = new JSONObject(data);
-
-        return objectMapper.readValue(jsonObject.toString(), Credentials.class);
-    }
-
-    public static Credentials[] getCredentialsArray(String name) throws FileNotFoundException {
-
-        String data = Service.fetchTestdata(name);
-        JSONArray jsonArray = new JSONArray(data);
-
-        return IntStream.range(0, jsonArray.length())
-                .mapToObj(i -> {
-                    try {
-                        return objectMapper.readValue(jsonArray.getJSONObject(i).toString(), Credentials.class);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .toArray(Credentials[]::new);
-    }
-
-    public static LinkData[] getLinkDataArray(String key) throws FileNotFoundException {
-
-        String data = LocalTestdataProvider.fetchTestdata(LocalTestdata.LINKS);
-        JSONObject jsonObject = new JSONObject(data);
-
+        JSONObject jsonObject = new JSONObject(json);
         JSONArray jsonArray = jsonObject.getJSONArray(key);
 
         return IntStream.range(0, jsonArray.length())
-                .mapToObj(i -> {
-                    try {
-                        return objectMapper.readValue(jsonArray.getJSONObject(i).toString(), LinkData.class);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
+                .mapToObj(i -> new Credentials(
+                        jsonArray.getJSONObject(i).getString("email"),
+                        jsonArray.getJSONObject(i).getString("password")
+                ))
+                .toArray(Credentials[]::new);
+    }
+
+    public static LinkData[] getLinkData(String json, String key) throws FileNotFoundException {
+
+        JSONObject jsonObject = new JSONObject(json);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new LinkData(
+                        jsonArray.getJSONObject(i).getString("link"),
+                        jsonArray.getJSONObject(i).getString("url")
+                ))
                 .toArray(LinkData[]::new);
     }
 
-    public static NewsletterData[] getNewsletterDataArray(String name) throws FileNotFoundException {
+    public static NewsletterData[] getNewsletterData(String json, String key) throws FileNotFoundException {
 
-        String data = Service.fetchTestdata(name);
-        JSONArray jsonArray = new JSONArray(data);
+        JSONObject jsonObject = new JSONObject(json);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
 
         return IntStream.range(0, jsonArray.length())
-                .mapToObj(i -> {
-                    try {
-                        return objectMapper.readValue(jsonArray.getJSONObject(i).toString(), NewsletterData.class);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
+                .mapToObj(i -> new NewsletterData(jsonArray.getJSONObject(i).getString("email")))
                 .toArray(NewsletterData[]::new);
+    }
+
+    public static String[] getPhrases(String json, String key) throws FileNotFoundException {
+
+        JSONObject jsonObject = new JSONObject(json);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(jsonArray::getString)
+                .toArray(String[]::new);
+    }
+
+    public static QuantityData[] getQuantityData(String json, String key) {
+
+        JSONObject jsonObject = new JSONObject(json);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new QuantityData(
+                        String.valueOf(jsonArray.getJSONObject(i).getString("quantity")),
+                        jsonArray.getJSONObject(i).getString("message")
+                ))
+                .toArray(QuantityData[]::new);
     }
 }
