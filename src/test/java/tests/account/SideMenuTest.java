dbad13e2d0e25c22c8886a95b696fef03f9dbae5
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-01 11:27:13 +0100

    Removed @BeforeSuite and @AfterSuite methods

--- src/test/java/tests/account/SideMenuTest.java
+++ src/test/java/tests/account/SideMenuTest.java
@@ -1,42 +1,23 @@
 package tests.account;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.dataProviders.DataProviders;
 import qa.dataProviders.SideMenuDataProviders;
-import qa.enums.DataDownloadMode;
 import qa.enums.URLs;
 import qa.exceptions.MockarooRequestException;
 import qa.helpers.LoginActions;
 import qa.pageobject.accountpage.AccountPage;
 import qa.records.LinkData;
 import qa.support.dataprovidernames.DataProviderNames;
-import qa.support.testdatafilenames.TestdataFileNames;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 public class SideMenuTest extends BaseTest {
 
     private AccountPage accountPage;
-
-    @BeforeSuite
-    public void loadTestdata() throws MalformedURLException, FileNotFoundException, URISyntaxException, MockarooRequestException {
-
-        SideMenuDataProviders.loadTestdata(TestdataFileNames.LINKS, DataDownloadMode.LOCAL);
-    }
-
-    @AfterSuite
-    public void clearTestdata() {
-
-        SideMenuDataProviders.clear();
-    }
 
     @BeforeMethod
     public void create() throws IOException, URISyntaxException, MockarooRequestException {
