0135e95f695a5ebcbabc491fcbf1ac803101c408
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-01 08:46:49 +0100

    Removed CredentialsDataProviders

--- src/main/java/qa/helpers/LoginActions.java
+++ src/main/java/qa/helpers/LoginActions.java
@@ -1,12 +1,8 @@
 package qa.helpers;
 
 import com.microsoft.playwright.Page;
-import qa.dataProviders.CredentialsDataProviders;
-import qa.enums.DataDownloadMode;
 import qa.exceptions.MockarooRequestException;
 import qa.pageobject.components.LoginForm;
-import qa.records.Credentials;
-import qa.support.testdatafilenames.TestdataFileNames;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,13 +11,12 @@ public class LoginActions {
 
     public static void login(Page page) throws IOException, URISyntaxException, MockarooRequestException {
 
-        CredentialsDataProviders.loadTestdata(TestdataFileNames.CREDENTIALS, DataDownloadMode.LOCAL);
-        CredentialsDataProviders dataProviders = new CredentialsDataProviders();
-        Credentials[] credentials = (Credentials[]) dataProviders.correct();
+        String email = "email";
+        String password = "password";
 
         LoginForm loginForm = new LoginForm(page);
-        loginForm.setEmail(credentials[0].getEmail());
-        loginForm.setPassword(credentials[0].getPassword());
+        loginForm.setEmail(email);
+        loginForm.setPassword(password);
         loginForm.clickLoginButton();
     }
 }
