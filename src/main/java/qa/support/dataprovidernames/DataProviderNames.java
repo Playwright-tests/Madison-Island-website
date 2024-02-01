c9c9de060a1774a768738474aff548a5cef54950
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-01 08:49:22 +0100

    Added fields

--- src/main/java/qa/support/dataprovidernames/DataProviderNames.java
+++ src/main/java/qa/support/dataprovidernames/DataProviderNames.java
@@ -1,15 +1,12 @@
 package qa.support.dataprovidernames;
 
 public class DataProviderNames {
-
-    public static final String LINKS = "links";
     public static final String WOMEN = "women";
     public static final String MEN = "men";
     public static final String ACCESSORIES = "accessories";
     public static final String HOME_DECOR = "homeDecor";
     public static final String SALE = "sale";
     public static final String VIP = "VIP";
-
     public static final String CORRECT = "correct";
     public static final String INCORRECT_EMAIL = "incorrectEmail";
     public static final String INCORRECT_PASSWORD = "incorrectPassword";
@@ -25,4 +22,14 @@ public class DataProviderNames {
     public static final String LOWER_UPPER = "lowerUpper";
     public static final String INCORRECT = "incorrect";
     public static final String NAUGHTY_STRINGS = "naughtyString";
+    public static final String SIDE_MENU = "sideMenu";
+    public static final String BELOW_MIN = "belowMin";
+    public static final String MIN = "min";
+    public static final String ABOVE_MIN = "aboveMin";
+    public static final String BELOW_MAX = "belowMax";
+    public static final String MAX = "max";
+    public static final String ABOVE_MAX = "aboveMax";
+    public static final String NEGATIVE = "negative";
+    public static final String FLOATING_POINT = "floatingPoint";
+    public static final String NOT_NUMBER = "notNumber";
 }
