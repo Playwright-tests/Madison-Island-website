35498845f6743df6cec851ae121a781c857525d5
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-02 08:51:46 +0100

    Created

---
+++ src/main/java/qa/dataProviders/ProductDataProviders.java
@@ -0,0 +1,15 @@
+package qa.dataProviders;
+
+import org.testng.annotations.DataProvider;
+import qa.base.BaseDataProviders;
+import qa.json.TestdataLoader;
+import qa.support.dataprovidernames.DataProviderNames;
+
+public class ProductDataProviders extends BaseDataProviders {
+
+    @DataProvider(name = DataProviderNames.PRODUCT_ATTRIBUTES)
+    public Object[] productAttributes() {
+
+        return TestdataLoader.getProductData(getJsonFile());
+    }
+}
