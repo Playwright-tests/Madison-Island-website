59aecb776e956cad93f5b67f1bfabcb0524b78be
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-02 18:35:44 +0100

    Removed the ExtentReports

--- src/test/java/tests/shoppingcart/LinkToShoppingCartTest.java
+++ src/test/java/tests/shoppingcart/LinkToShoppingCartTest.java
@@ -3,16 +3,12 @@ package tests.shoppingcart;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.extentreportsmanager.ExtentReportsManager;
 import qa.pageobject.sections.Header;
 
 public class LinkToShoppingCartTest extends BaseTest {
 
     @Test
     public void clickingTheLink() throws InterruptedException {
-
-        ExtentReportsManager.createTest("Clicking the \"Cart\" link",
-                "Checking whether the mini cart opens after clicking the \"Cart\" link.");
 
         Header header = new Header(getPage());
         header.clickShoppingCartLink();
