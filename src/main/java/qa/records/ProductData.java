0d6a7eee1613ae4887248746991c475bb26e3620
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-03 11:04:59 +0100

    Added field

--- src/main/java/qa/records/ProductData.java
+++ src/main/java/qa/records/ProductData.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class ProductData {
 
     private final String url;
+    private final String name;
     private final String color;
     private final String size;
     private final String quantity;
@@ -13,14 +14,16 @@ public class ProductData {
     public ProductData() {
 
         this.url = null;
+        this.name = null;
         this.color = null;
         this.size = null;
         this.quantity = null;
     }
 
-    public ProductData(String url, String color, String size, String quantity) {
+    public ProductData(String url, String name, String color, String size, String quantity) {
 
         this.url = url;
+        this.name = name;
         this.color = color;
         this.size = size;
         this.quantity = quantity;
