2dbec783d0646558d87e570df0f6baeb71a65c46
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-04 07:08:14 +0100

    Created

---
+++ src/main/java/qa/dataProviders/CouponCodeDataProviders.java
@@ -0,0 +1,17 @@
+package qa.dataProviders;
+
+import org.testng.annotations.DataProvider;
+import qa.base.BaseDataProviders;
+import qa.json.TestdataLoader;
+import qa.support.dataprovidernames.DataProviderNames;
+
+import java.io.FileNotFoundException;
+
+public class CouponCodeDataProviders extends BaseDataProviders {
+
+    @DataProvider(name = DataProviderNames.INCORRECT)
+    public Object[] lowerUpper() throws FileNotFoundException {
+
+        return TestdataLoader.getStrings(getJsonFile(), DataProviderNames.INCORRECT);
+    }
+}
