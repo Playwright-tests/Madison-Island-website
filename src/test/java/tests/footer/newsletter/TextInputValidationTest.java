932fc52af6496d4432158d9f68130065bd248965
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-04 10:38:45 +0100

    Created the test

---
+++ src/test/java/tests/footer/newsletter/TextInputValidationTest.java
@@ -0,0 +1,28 @@
+package tests.footer.newsletter;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import qa.base.BaseTest;
+import qa.pageobject.footer.NewsletterForm;
+
+public class TextInputValidationTest extends BaseTest {
+
+    private NewsletterForm newsletterForm;
+
+    @BeforeMethod
+    public void create() {
+
+        newsletterForm = new NewsletterForm(getPage());
+    }
+
+    @Test
+    public void emailField() {
+
+        String email = "example@gmail.com";
+        newsletterForm.setEmail(email);
+
+        Assert.assertEquals(newsletterForm.getEmailFieldText(), email,
+                "Incorrect input content");
+    }
+}
