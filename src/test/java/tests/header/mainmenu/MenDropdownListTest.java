a3b669af7ec451af0f26154cb4d8d341c2f0f668
pawelaksman <pablo_xmen@op.pl>
2024-04-30 23:42:27 +0200

    Added methods

--- src/test/java/tests/header/mainmenu/MenDropdownListTest.java
+++ src/test/java/tests/header/mainmenu/MenDropdownListTest.java
@@ -1,4 +1,48 @@
 package tests.header.mainmenu;
 
-public class MenDropdownListTest {
+import org.testng.annotations.Test;
+import tests.base.MainMenuBaseTest;
+
+public class MenDropdownListTest extends MainMenuBaseTest {
+
+    public MenDropdownListTest() {
+
+        setTriggerElement("Men");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void viewAllMenLink() {
+
+        actions("View All Men", "http://demo-store.seleniumacademy.com/men.html");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void newArrivalsLink() {
+
+        actions("New Arrivals", "http://demo-store.seleniumacademy.com/men/new-arrivals.html");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void shirtsLink() {
+
+        actions("Shirts", "http://demo-store.seleniumacademy.com/men/shirts.html");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void teesKnitsLink() {
+
+        actions("Tees, Knits and Polos", "http://demo-store.seleniumacademy.com/men/tees-knits-and-polos.html");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void pantsAndDenim() {
+
+        actions("Pants & Denim", "http://demo-store.seleniumacademy.com/men/pants-denim.html");
+    }
+
+    @Test(groups = "withExpandedList")
+    public void blazersLink() {
+
+        actions("Blazers", "http://demo-store.seleniumacademy.com/men/blazers.html");
+    }
 }
