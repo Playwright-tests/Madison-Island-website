b44dadbb631b7d77d9d33926d55eae418c847757
pawelaksman <pablo_xmen@op.pl>
2024-05-01 15:58:41 +0200

    Created

---
+++ src/main/java/qa/support/URLs.java
@@ -0,0 +1,37 @@
+package qa.support;
+
+public class URLs {
+    public static final String HOME_PAGE = "http://demo-store.seleniumacademy.com/";
+    public static final String ELIZABETH_KNIT_PAGE = "http://demo-store.seleniumacademy.com/women/tops-blouses/elizabeth-knit-top-493.html";
+    public static final String SHOPPING_CART = "http://demo-store.seleniumacademy.com/checkout/cart/";
+    public static final String LOGIN_PAGE = "http://demo-store.seleniumacademy.com/customer/account/login/";
+    public static final String NEWSLETTER_SUBSCRIPTION_PAGE = "http://demo-store.seleniumacademy.com/newsletter/subscriber/new/";
+    public static final String SEARCH_RESULTS_PAGE = "http://demo-store.seleniumacademy.com/catalogsearch/result/";
+    public static final String WOMEN_PAGE = "http://demo-store.seleniumacademy.com/women.html";
+    public static final String MEN_PAGE = "http://demo-store.seleniumacademy.com/men.html";
+    public static final String ACCESSORIES_PAGE = "http://demo-store.seleniumacademy.com/accessories.html";
+    public static final String HOME_DECOR_PAGE = "http://demo-store.seleniumacademy.com/home-decor.html";
+    public static final String SALE_PAGE = "http://demo-store.seleniumacademy.com/sale.html";
+    public static final String VIP_PAGE = "http://demo-store.seleniumacademy.com/vip.html";
+    public static final String EYEWEAR_PAGE = "http://demo-store.seleniumacademy.com/accessories/eyewear.html";
+    public static final String JEWELRY_PAGE = "http://demo-store.seleniumacademy.com/accessories/jewelry.html";
+    public static final String SHOES_PAGE = "http://demo-store.seleniumacademy.com/accessories/shoes.html";
+    public static final String BAGS_LUGGAGE_PAGE = "http://demo-store.seleniumacademy.com/accessories/bags-luggage.html";
+    public static final String BOOKS_MUSIC_PAGE = "http://demo-store.seleniumacademy.com/home-decor/books-music.html";
+    public static final String BED_BATH_PAGE = "http://demo-store.seleniumacademy.com/home-decor/bed-bath.html";
+    public static final String ELECTRONICS_PAGE = "http://demo-store.seleniumacademy.com/home-decor/electronics.html";
+    public static final String DECORATIVE_ACCENTS_PAGE = "http://demo-store.seleniumacademy.com/home-decor/decorative-accents.html";
+    public static final String MEN_NEW_ARRIVALS_PAGE = "http://demo-store.seleniumacademy.com/men/new-arrivals.html";
+    public static final String SHIRTS_PAGE = "http://demo-store.seleniumacademy.com/men/shirts.html";
+    public static final String TEES_PAGE = "http://demo-store.seleniumacademy.com/men/tees-knits-and-polos.html";
+    public static final String MEN_PANTS_DENIM_PAGE = "http://demo-store.seleniumacademy.com/men/pants-denim.html";
+    public static final String BLAZERS_PAGE = "http://demo-store.seleniumacademy.com/men/blazers.html";
+    public static final String SALE_WOMEN_PAGE = "http://demo-store.seleniumacademy.com/sale/women.html";
+    public static final String SALE_MEN_PAGE = "http://demo-store.seleniumacademy.com/sale/men.html";
+    public static final String SALE_ACCESSORIES_PAGE = "http://demo-store.seleniumacademy.com/sale/accessories.html";
+    public static final String SALE_HOME_DECOR_PAGE = "http://demo-store.seleniumacademy.com/sale/home-decor.html";
+    public static final String WOMEN_NEW_ARRIVALS_PAGE = "http://demo-store.seleniumacademy.com/women/new-arrivals.html";
+    public static final String TOPS_BLOUSES_PAGE = "http://demo-store.seleniumacademy.com/women/tops-blouses.html";
+    public static final String WOMEN_PANTS_DENIM_PAGE = "http://demo-store.seleniumacademy.com/women/pants-denim.html";
+    public static final String DRESSES_SKIRTS_PAGE = "http://demo-store.seleniumacademy.com/women/dresses-skirts.html";
+}
