a0445b929ff248b5d0871c95ce300d1634970d93
pawelaksman <pablo_xmen@op.pl>
2024-05-03 00:26:25 +0200

    Added methods

--- src/main/java/qa/pageobject/header/AccountDropdownList.java
+++ src/main/java/qa/pageobject/header/AccountDropdownList.java
@@ -3,28 +3,60 @@ package qa.pageobject.header;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import lombok.Getter;
 import qa.base.BasePage;
 
+@Getter
 public class AccountDropdownList extends BasePage {
 
-    private final Locator nav;
+    private final String listSelector;
 
     public AccountDropdownList(Page page) {
 
         super(page);
 
-        nav = getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Account").setExact(true));
+        listSelector = "#header-account";
     }
 
-    public void clickAccountNav() {
-
-        nav.click();
-    }
-
-    public void clickItem(String nth) {
+    public void clickItem(String name) {
 
         getPage().locator("#header-account")
-                 .getByRole(AriaRole.LINK, new Locator
-                 .GetByRoleOptions().setName(nth).setExact(true)).click();
+                .getByRole(AriaRole.LINK, new Locator
+                        .GetByRoleOptions().setName(name).setExact(true)).click();
+    }
+
+    public void clickTriggerElement() {
+
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Account").setExact(true)).click();
+    }
+
+    public void clickMyAccountLink() {
+
+        clickItem("My Account");
+    }
+
+    public void clickMyWishlistLink() {
+
+        clickItem("My Wishlist");
+    }
+
+    public void clickMyCartLink() {
+
+        clickItem("My Cart");
+    }
+
+    public void clickCheckoutLink() {
+
+        clickItem("Checkout");
+    }
+
+    public void clickRegisterLink() {
+
+        clickItem("Register");
+    }
+
+    public void clickLoginLink() {
+
+        clickItem("Log In");
     }
 }
