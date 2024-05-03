fe2bd34357bc91ef38c8393099ec5cdd239ffc6b
pawelaksman <pablo_xmen@op.pl>
2024-05-03 23:53:28 +0200

    Moved

--- src/main/java/qa/support/TestDataLoader.java
+++ src/main/java/qa/support/TestDataLoader.java
@@ -1,36 +1,35 @@
-package qa.IO;
+package qa.support;
 
-import org.json.JSONObject;
+import lombok.Getter;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
-public class LocalTestdataProvider {
+public class TestDataLoader {
 
-    private static final Logger logger = LoggerFactory.getLogger(LocalTestdataProvider.class);
-    private static final String PATH = "./src/main/resources/";
+    private static final Logger logger = LoggerFactory.getLogger(TestDataLoader.class);
+    private static final String PATH = "./src/test/resources/testdata/";
+    @Getter
+    private static String source;
 
-    public static String fetchTestdata(String fileName) throws FileNotFoundException {
+    public static void load(String fileName) {
 
-        String fullPath = PATH + fileName + ".json";
+        String fullPath = PATH + fileName;
 
         try {
             Reader reader = new FileReader(fullPath);
             JSONParser jsonParser = new JSONParser();
             Object object = jsonParser.parse(reader);
             logger.info("Downloading testdata locally...");
-
-            return object.toString();
+            source = object.toString();
 
         } catch (IOException | ParseException e) {
 
-            logger.info("Could not load the testdata from path: " + fullPath);
+            logger.info("Could not load the testdata from path: {}", fullPath);
             throw new RuntimeException(e);
         }
 
