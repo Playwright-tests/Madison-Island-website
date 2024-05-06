7403d7c8aea35be6a29989fb2e21bd3643fd1e5e
pawelaksman <pablo_xmen@op.pl>
2024-05-06 22:51:53 +0200

    Updated the import

--- src/test/java/tests/header/searchengine/InputTextValidationTest.java
+++ src/test/java/tests/header/searchengine/InputTextValidationTest.java
@@ -3,7 +3,7 @@ package tests.header.searchengine;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import qa.base.BaseTest;
+import tests.base.BaseTest;
 import qa.pageobject.header.SearchEngine;
 
 public class InputTextValidationTest extends BaseTest {
