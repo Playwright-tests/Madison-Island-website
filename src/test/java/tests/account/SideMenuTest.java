4a707639e68f65c3734225582d141815f4bc8932
pawelaksman <pablo_xmen@op.pl>
2024-05-09 23:27:42 +0200

    Added test methods

--- src/test/java/tests/account/SideMenuTest.java
+++ src/test/java/tests/account/SideMenuTest.java
@@ -1,39 +1,91 @@
 package tests.account;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import qa.base.BaseTest;
-import qa.dataProviders.SideMenuDataProviders;
-import qa.enums.URLs;
-import qa.exceptions.MockarooRequestException;
-import qa.helpers.LoginActions;
-import qa.pageobject.accountpage.AccountPage;
-import qa.records.LinkData;
-import qa.support.dataprovidernames.DataProviderNames;
+import qa.pageobject.accountpage.SideMenu;
+import tests.base.BaseTest;
+import qa.support.URLs;
+import qa.helpers.Authentication;
+
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class SideMenuTest extends BaseTest {
 
-    private AccountPage accountPage;
+    private SideMenu sideMenu;
 
     @BeforeMethod
-    public void create() throws IOException, URISyntaxException, MockarooRequestException {
+    public void prepare() throws IOException, URISyntaxException {
 
-        goToPage(URLs.LOGIN_PAGE.getName());
-        LoginActions.login(getPage());
-
-        accountPage = new AccountPage(getPage());
+        goToPage(URLs.LOGIN_PAGE);
+        Authentication.login(getPage());
+        sideMenu = new SideMenu(getPage());
     }
 
-    @Test(dataProvider = DataProviderNames.SIDE_MENU, dataProviderClass = SideMenuDataProviders.class)
-    public void links(LinkData linkData) {
+    @Test
+    public void accountDashboardLink() {
 
-        accountPage.getSideMenu().clickLink(linkData.getLink());
+        clickAndVerifyPageUrl(SideMenu::clickAccountDashboardLink, sideMenu, URLs.ACCOUNT_DASHBOARD_PAGE);
+    }
 
-        Assert.assertEquals(getPage().url(), linkData.getUrl(),
-                "The page with address \"" + linkData.getUrl() + "\" has not been opened");
+    @Test
+    public void accountInformationLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickAccountInformationLink, sideMenu, URLs.ACCOUNT_INFORMATION_PAGE);
+    }
+
+    @Test
+    public void addressBookLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickAddressBookLink, sideMenu, URLs.ADDRESS_BOOK_PAGE);
+    }
+
+    @Test
+    public void myOrdersLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickMyOrdersLink, sideMenu, URLs.MY_ORDERS_PAGE);
+    }
+
+    @Test
+    public void billingAgreementsLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickBillingAgreementsLink, sideMenu, URLs.BILLING_AGREEMENTS_PAGE);
+    }
+
+    @Test
+    public void recurringProfilesLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickRecurringProfilesLink, sideMenu, URLs.RECURRING_PROFILE_PAGE);
+    }
+
+    @Test
+    public void myProductReviews() {
+
+        clickAndVerifyPageUrl(SideMenu::clickMyProductReviewsLink, sideMenu, URLs.MY_PRODUCT_REVIEWS_PAGE);
+    }
+
+    @Test
+    public void myWishlistLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickMyWishlistLink, sideMenu, URLs.MY_WISHLIST_PAGE);
+    }
+
+    @Test
+    public void myApplicationsLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickMyApplicationsLink, sideMenu, URLs.MY_APPLICATIONS_PAGE);
+    }
+
+    @Test
+    public void newsletterSubscriptionsLink() {
+
+        clickAndVerifyPageUrl(SideMenu::clickNewsletterSubscriptionsLink, sideMenu, URLs.ACCOUNT_NEWSLETTER_SUBSCRIPTION_PAGE);
+    }
+
+    @Test
+    public void myDownloadableProducts() {
+
+        clickAndVerifyPageUrl(SideMenu::clickMyDownloadableProductsLink, sideMenu, URLs.MY_DOWNLOADABLE_PAGE);
     }
 }
