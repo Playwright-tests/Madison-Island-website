110989d2c7d73e8922404b71c6bfe7fc98e97e2a
pawelaksman <pablo_xmen@op.pl>
2024-05-09 23:26:07 +0200

    Added test methods

--- src/main/java/qa/pageobject/accountpage/SideMenu.java
+++ src/main/java/qa/pageobject/accountpage/SideMenu.java
@@ -11,8 +11,63 @@ public class SideMenu extends BasePage {
         super(page);
     }
 
-    public void clickLink(String linkText) {
+    private void clickLink(String linkText) {
 
         getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(linkText)).click();
+    }
+
+    public void clickAccountDashboardLink() {
+
+        clickLink("Account Dashboard");
+    }
+
+    public void clickAccountInformationLink() {
+
+        clickLink("Account Information");
+    }
+
+    public void clickAddressBookLink() {
+
+        clickLink("Address Book");
+    }
+
+    public void clickMyOrdersLink() {
+
+        clickLink("My Orders");
+    }
+
+    public void clickBillingAgreementsLink() {
+
+        clickLink("Billing Agreements");
+    }
+
+    public void clickRecurringProfilesLink() {
+
+        clickLink("Recurring Profiles");
+    }
+
+    public void clickMyProductReviewsLink() {
+
+        clickLink("My Product Reviews");
+    }
+
+    public void clickMyWishlistLink() {
+
+        clickLink("My Wishlist");
+    }
+
+    public void clickMyApplicationsLink() {
+
+        clickLink("My Applications");
+    }
+
+    public void clickNewsletterSubscriptionsLink() {
+
+        clickLink("Newsletter Subscriptions");
+    }
+
+    public void clickMyDownloadableProductsLink() {
+
+        clickLink("My Downloadable Products");
     }
 }
