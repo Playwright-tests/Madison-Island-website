3837f43238af011aa707d8e148d70a2c17884ba4
pawelaksman <pablo_xmen@op.pl>
2024-05-09 23:30:44 +0200

    Removed methods

--- src/main/java/qa/pageobject/sections/Header.java
+++ src/main/java/qa/pageobject/sections/Header.java
@@ -3,44 +3,21 @@ package qa.pageobject.sections;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import qa.pageobject.header.MainMenu;
-import qa.pageobject.header.SearchEngine;
 import qa.base.BasePage;
 
 public class Header extends BasePage {
 
-    private final MainMenu mainMenu;
-    private final SearchEngine searchEngine;
     private final Locator shoppingCartLink;
-    private final Locator minicartWrapper;
 
     public Header(Page page) {
 
         super(page);
 
-        mainMenu = new MainMenu(getPage());
-        searchEngine = new SearchEngine(getPage());
         shoppingCartLink = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Cart"));
-        minicartWrapper = page.locator(".minicart-wrapper");
     }
 
     public void clickShoppingCartLink() {
 
         shoppingCartLink.click();
-    }
-
-    public Locator getMinicartWrapper() {
-
-        return minicartWrapper;
-    }
-
-    public MainMenu getMainMenu() {
-
-        return mainMenu;
-    }
-
-    public SearchEngine getSearchEngine() {
-
-        return searchEngine;
     }
 }
