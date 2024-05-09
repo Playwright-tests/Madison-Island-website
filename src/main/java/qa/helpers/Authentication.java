a7ea156ad876a5723d853134e7aa52f72136a80b
pawelaksman <pablo_xmen@op.pl>
2024-05-09 23:22:45 +0200

    Renamed

--- src/main/java/qa/helpers/Authentication.java
+++ src/main/java/qa/helpers/Authentication.java
@@ -1,15 +1,11 @@
 package qa.helpers;
 
 import com.microsoft.playwright.Page;
-import qa.exceptions.MockarooRequestException;
 import qa.pageobject.loginform.LoginForm;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+public class Authentication {
 
-public class LoginActions {
-
-    public static void login(Page page) throws IOException, URISyntaxException, MockarooRequestException {
+    public static void login(Page page) {
 
         String email = "goo@gmail.com";
         String password = "pawelek1234";
