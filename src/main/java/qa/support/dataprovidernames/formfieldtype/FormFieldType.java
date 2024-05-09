922bc7deb846c5d727bc1c7017c6319463601352
pawelaksman <pablo_xmen@op.pl>
2024-05-09 23:23:33 +0200

    Removed

--- src/main/java/qa/support/dataprovidernames/formfieldtype/FormFieldType.java
+++
@@ -1,7 +0,0 @@
-package qa.support.dataprovidernames.formfieldtype;
-
-public class FormFieldType {
-
-    public static final String FIELD = "field";
-    public static final String DROPDOWN_LIST = "dropdownList";
-}
